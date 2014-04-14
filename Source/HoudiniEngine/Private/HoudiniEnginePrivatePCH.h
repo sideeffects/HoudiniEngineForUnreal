dff0d95b6b912909b9ca861680344b8b13d7b224
Damian Campeanu <damian@sidefx.com>
2014-04-14 19:22:21 -0400

    Added copyright headers.

--- Source/HoudiniEngine/Private/HoudiniEnginePrivatePCH.h
+++ Source/HoudiniEngine/Private/HoudiniEnginePrivatePCH.h
@@ -1,5 +1,16 @@
-// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
-
+/*
+ * PROPRIETARY INFORMATION.  This software is proprietary to
+ * Side Effects Software Inc., and is not to be reproduced,
+ * transmitted, or disclosed in any way without written permission.
+ *
+ * Produced by:
+ *      Side Effects Software Inc
+ *      123 Front Street West, Suite 1401
+ *      Toronto, Ontario
+ *      Canada   M5J 2M2
+ *      416-504-9876
+ *
+ */
 #pragma once
 
 #define HOUDINI_DEBUG_LOGGING 0
