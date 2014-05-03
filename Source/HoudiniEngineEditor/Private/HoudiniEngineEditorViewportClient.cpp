a18f24bbbf37367adcf2b4c43c324c7644635244
Mykola Konyk <mykola@sidefx.com>
2014-05-03 17:38:56 -0400

    Adding Editor files and modifying .gitignore.

---
+++ Source/HoudiniEngineEditor/Private/HoudiniEngineEditorViewportClient.cpp
@@ -0,0 +1,27 @@
+/*
+* PROPRIETARY INFORMATION.  This software is proprietary to
+* Side Effects Software Inc., and is not to be reproduced,
+* transmitted, or disclosed in any way without written permission.
+*
+* Produced by:
+*      Mykola Konyk
+*      Side Effects Software Inc
+*      123 Front Street West, Suite 1401
+*      Toronto, Ontario
+*      Canada   M5J 2M2
+*      416-504-9876
+*
+*/
+
+#include "HoudiniEngineEditorPrivatePCH.h"
+#if 0
+
+//--
+FHoudiniEngineEditorViewportClient::FHoudiniEngineEditorViewportClient(TWeakPtr<IHoudiniEngineEditor> inHoudiniENgineEditor, FPreviewScene& InPreviewScene, UHoudiniEngineAsset* inHoudiniEngineAsset, UHoudiniEngineAssetComponent* inUHoudiniEngineAssetComponent) :
+	FEditorViewportClient(&InPreviewScene),
+	HoudiniEngineEditor(inHoudiniENgineEditor)
+{
+
+}
+
+#endif
