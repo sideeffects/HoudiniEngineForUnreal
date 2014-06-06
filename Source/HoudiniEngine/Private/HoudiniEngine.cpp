6befde36bfd7a93e39c57cdabe0de93e51f14bee
Damian Campeanu <damian@sidefx.com>
2014-06-06 11:38:40 -0400

    Added max vertices per primitive setting to fix problems with quad geometry.

--- Source/HoudiniEngine/Private/HoudiniEngine.cpp
+++ Source/HoudiniEngine/Private/HoudiniEngine.cpp
@@ -40,6 +40,7 @@ FHoudiniEngine::StartupModule()
 	
 	// Perform HAPI initialization.
 	HAPI_CookOptions CookOptions = HAPI_CookOptions_Create();
+	CookOptions.maxVerticesPerPrimitive = 3;
 	HAPI_Result Result = HAPI_Initialize("", "", CookOptions, true, -1);
 
 	if(HAPI_RESULT_SUCCESS == Result)
