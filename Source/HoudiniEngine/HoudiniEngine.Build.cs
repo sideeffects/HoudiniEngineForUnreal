050aa0d6586a3c0be809a2437ec94941c2cdfb0d
Mykola Konyk <mykola@sidefx.com>
2014-09-26 14:47:30 -0400

    Bumping to Houdini Version: 13.0.548 Houdini Engine Version: 1.8 (API: 16)

--- Source/HoudiniEngine/HoudiniEngine.Build.cs
+++ Source/HoudiniEngine/HoudiniEngine.Build.cs
@@ -16,7 +16,7 @@
 
 /*
 
-	Houdini Version: 13.0.547
+	Houdini Version: 13.0.548
 	Houdini Engine Version: 1.8.16
 	Unreal Version: 4.4.1
 
@@ -30,13 +30,13 @@ namespace UnrealBuildTool.Rules
 		{
 			Definitions.Add("HOUDINIENGINE_ASSET_SCRATCHSPACE_SIZE=65536");
 
-			PublicLibraryPaths.Add("C:/Program Files/Side Effects Software/Houdini 13.0.547/custom/houdini/dsolib");
-			PublicAdditionalLibraries.Add("C:/Program Files/Side Effects Software/Houdini 13.0.547/custom/houdini/dsolib/libHAPI.a");
+			PublicLibraryPaths.Add("C:/Program Files/Side Effects Software/Houdini 13.0.548/custom/houdini/dsolib");
+			PublicAdditionalLibraries.Add("C:/Program Files/Side Effects Software/Houdini 13.0.548/custom/houdini/dsolib/libHAPI.a");
 
 			PublicIncludePaths.AddRange(
 				new string[] {
 					// ... add public include paths required here ...
-					"C:/Program Files/Side Effects Software/Houdini 13.0.547/toolkit/include/HAPI"
+					"C:/Program Files/Side Effects Software/Houdini 13.0.548/toolkit/include/HAPI"
 				}
 				);
 
