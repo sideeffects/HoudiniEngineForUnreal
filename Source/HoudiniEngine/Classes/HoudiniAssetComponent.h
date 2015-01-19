93e070c51124397b8872c52d76492fb21d221c50
Mykola Konyk <mykola@sidefx.com>
2015-01-19 11:02:44 -0500

    Fixing Houdini component comments for generated properties as it seems Unreal Header Tool parser is expecting certain format.

--- Source/HoudiniEngine/Classes/HoudiniAssetComponent.h
+++ Source/HoudiniEngine/Classes/HoudiniAssetComponent.h
@@ -79,23 +79,23 @@ class HOUDINIENGINE_API UHoudiniAssetComponent : public UPrimitiveComponent
 /** Static mesh generation properties.**/
 public:
 
-	/** If true, the physics triangle mesh will use double sided faces when doing scene queries. **/
+	/** If true, the physics triangle mesh will use double sided faces when doing scene queries. */
 	UPROPERTY(EditAnywhere, Category=HoudiniGeneratedStaticMeshSettings, meta=(DisplayName="Double Sided Geometry"))
 	uint32 bGeneratedDoubleSidedGeometry : 1;
 
-	/** Physical material to use for simple collision on this body. Encodes information about density, friction etc. **/
+	/** Physical material to use for simple collision on this body. Encodes information about density, friction etc. */
 	UPROPERTY(EditAnywhere, Category=HoudiniGeneratedStaticMeshSettings, meta=(DisplayName="Simple Collision Physical Material"))
 	UPhysicalMaterial* GeneratedPhysMaterial;
 
-	/** Collision Trace behavior - by default, it will keep simple(convex)/complex(per-poly) separate. **/
+	/** Collision Trace behavior - by default, it will keep simple(convex)/complex(per-poly) separate. */
 	UPROPERTY(EditAnywhere, Category=HoudiniGeneratedStaticMeshSettings, meta=(DisplayName="Collision Complexity"))
 	TEnumAsByte<enum ECollisionTraceFlag> GeneratedCollisionTraceFlag;
 
-	/** Resolution of lightmap. **/
+	/** Resolution of lightmap. */
 	UPROPERTY(EditAnywhere, Category=HoudiniGeneratedStaticMeshSettings, meta=(DisplayName="Light Map Resolution", FixedIncrement="4.0"))
 	int32 GeneratedLightMapResolution;
 
-	/** Bias multiplier for Light Propagation Volume lighting. **/
+	/** Bias multiplier for Light Propagation Volume lighting. */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=HoudiniGeneratedStaticMeshSettings, meta=(DisplayName="Lpv Bias Multiplier", UIMin="0.0", UIMax="3.0"))
 	float GeneratedLpvBiasMultiplier;
 
@@ -107,15 +107,15 @@ public:
 	UPROPERTY(EditAnywhere, AdvancedDisplay, Category=HoudiniGeneratedStaticMeshSettings, meta=(DisplayName="Light map coordinate index"))
 	int32 GeneratedLightMapCoordinateIndex;
 
-	/** True if mesh should use a less-conservative method of mip LOD texture factor computation. **/
+	/** True if mesh should use a less-conservative method of mip LOD texture factor computation. */
 	UPROPERTY(EditAnywhere, AdvancedDisplay, Category=HoudiniGeneratedStaticMeshSettings, meta=(DisplayName="Use Maximum Streaming Texel Ratio"))
 	uint32 bGeneratedUseMaximumStreamingTexelRatio:1;
 
-	/** Allows artists to adjust the distance where textures using UV 0 are streamed in/out. **/
+	/** Allows artists to adjust the distance where textures using UV 0 are streamed in/out. */
 	UPROPERTY(EditAnywhere, AdvancedDisplay, Category=HoudiniGeneratedStaticMeshSettings, meta=(DisplayName="Streaming Distance Multiplier"))
 	float GeneratedStreamingDistanceMultiplier;
 
-	/** Default settings when using this mesh for instanced foliage. **/
+	/** Default settings when using this mesh for instanced foliage. */
 	UPROPERTY(EditAnywhere, AdvancedDisplay, Instanced, Category=HoudiniGeneratedStaticMeshSettings, meta=(DisplayName="Foliage Default Settings"))
 	UFoliageType_InstancedStaticMesh* GeneratedFoliageDefaultSettings;
 
