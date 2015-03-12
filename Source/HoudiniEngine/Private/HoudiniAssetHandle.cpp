bdc6e1299f9aef6bfad3bb167082f9e09dc4d613
Mykola Konyk <mykola@sidefx.com>
2015-03-12 18:33:39 -0400

    Adding handle type, extracting necessary handle information.

--- Source/HoudiniEngine/Private/HoudiniAssetHandle.cpp
+++ Source/HoudiniEngine/Private/HoudiniAssetHandle.cpp
@@ -27,3 +27,53 @@ UHoudiniAssetHandle::~UHoudiniAssetHandle()
 {
 
 }
+
+
+UHoudiniAssetHandle*
+UHoudiniAssetHandle::Create(UHoudiniAssetComponent* InHoudiniAssetComponent, const HAPI_HandleInfo& HandleInfo,
+	int32 HandleIdx)
+{
+	UHoudiniAssetHandle* HoudiniAssetHandle = nullptr;
+
+	TArray<HAPI_HandleBindingInfo> BindingInfos;
+	BindingInfos.SetNumZeroed(HandleInfo.bindingsCount);
+
+	if(HAPI_RESULT_SUCCESS != FHoudiniApi::GetHandleBindingInfo(InHoudiniAssetComponent->GetAssetId(), HandleIdx,
+		&BindingInfos[0], 0, HandleInfo.bindingsCount))
+	{
+		return HoudiniAssetHandle;
+	}
+
+	TArray<HAPI_ParmId> ParameterIds;
+	TArray<FString> ParameterNames;
+
+	for(int32 HandleBindingIdx = 0; HandleBindingIdx < HandleInfo.bindingsCount; ++HandleBindingIdx)
+	{
+		const HAPI_HandleBindingInfo& HandleBindingInfo = BindingInfos[HandleBindingIdx];
+
+		// Store parameter id.
+		ParameterIds.Add(HandleBindingInfo.assetParmId);
+
+		FString ParmName;
+		if(!FHoudiniEngineUtils::GetHoudiniString(HandleBindingInfo.assetParmNameSH, ParmName))
+		{
+			return HoudiniAssetHandle;
+		}
+
+		ParameterNames.Add(ParmName);
+	}
+
+	HoudiniAssetHandle = NewObject<UHoudiniAssetHandle>(InHoudiniAssetComponent);
+
+	// Store handle index.
+	HoudiniAssetHandle->HandleIdx = HandleIdx;
+
+	// Set component and other information.
+	HoudiniAssetHandle->HoudiniAssetComponent = InHoudiniAssetComponent;
+
+	// Store controlled parameter ids and names.
+	HoudiniAssetHandle->ParameterIds = ParameterIds;
+	HoudiniAssetHandle->ParameterNames = ParameterNames;
+
+	return HoudiniAssetHandle;
+}
