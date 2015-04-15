e313366170f2082f416768c5ec06801529649922
Mykola Konyk <mykola@sidefx.com>
2015-04-15 17:12:37 -0400

    Adding HoudiniEngineEditor module in 4.7 branch.

---
+++ Source/HoudiniEngineEditor/Private/HoudiniEngineEditor.h
@@ -0,0 +1,51 @@
+/*
+ * PROPRIETARY INFORMATION.  This software is proprietary to
+ * Side Effects Software Inc., and is not to be reproduced,
+ * transmitted, or disclosed in any way without written permission.
+ *
+ * Produced by:
+ *      Mykola Konyk
+ *      Side Effects Software Inc
+ *      123 Front Street West, Suite 1401
+ *      Toronto, Ontario
+ *      Canada   M5J 2M2
+ *      416-504-9876
+ *
+ */
+
+#pragma once
+#include "IHoudiniEngineEditor.h"
+
+
+class UStaticMesh;
+
+class FHoudiniEngineEditor : public IHoudiniEngineEditor
+{
+
+/** IModuleInterface methods. **/
+public:
+
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+/** IHoudiniEngineEditor methods. **/
+public:
+
+public:
+
+	/** App identifier string. **/
+	static const FName HoudiniEngineEditorAppIdentifier;
+
+public:
+
+	/** Return singleton instance of Houdini Engine, used internally. **/
+	static FHoudiniEngineEditor& Get();
+
+	/** Return true if singleton instance has been created. **/
+	static bool IsInitialized();
+
+private:
+
+	/** Singleton instance of Houdini Engine runtime. **/
+	static FHoudiniEngineEditor* HoudiniEngineEditorInstance;
+};
