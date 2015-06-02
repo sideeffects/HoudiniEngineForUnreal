31b066b611da4b758847d6b5531f87e477d11c49
Mykola Konyk <mykola@sidefx.com>
2015-06-02 12:48:58 -0400

    Bumping to Houdini Version: 14.5.132 Houdini Engine Version: 1.10 (API: 9)

--- Source/HoudiniEngineEditor/HoudiniEngineEditor.Build.cs
+++ Source/HoudiniEngineEditor/HoudiniEngineEditor.Build.cs
@@ -16,7 +16,7 @@
 
 /*
 
-    Houdini Version: 14.5.128
+    Houdini Version: 14.5.132
     Houdini Engine Version: 1.10.9
     Unreal Version: 4.7.3
 
@@ -30,7 +30,7 @@ public class HoudiniEngineEditor : ModuleRules
 	public HoudiniEngineEditor( TargetInfo Target )
 	{
 		string HFSPath = "";
-		string HoudiniVersion = "14.5.128";
+		string HoudiniVersion = "14.5.132";
 
 		// Check if we are compiling on unsupported platforms.
 		if( Target.Platform != UnrealTargetPlatform.Win64 &&
