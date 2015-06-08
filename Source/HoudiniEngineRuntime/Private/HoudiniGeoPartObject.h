287f335edbdb89377d39483a682703b7325bab6c
Mykola Konyk <mykola@sidefx.com>
2015-06-08 18:08:59 -0400

    Changing export symbol name on Geo part object.

--- Source/HoudiniEngineRuntime/Private/HoudiniGeoPartObject.h
+++ Source/HoudiniEngineRuntime/Private/HoudiniGeoPartObject.h
@@ -170,4 +170,4 @@ public:
 
 
 /** Function used by hasing containers to create a unique hash for this type of object. **/
-HOUDINIENGINE_API uint32 GetTypeHash(const FHoudiniGeoPartObject& HoudiniGeoPartObject);
+HOUDINIENGINERUNTIME_API uint32 GetTypeHash(const FHoudiniGeoPartObject& HoudiniGeoPartObject);
