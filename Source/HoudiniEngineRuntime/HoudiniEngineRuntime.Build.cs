1414b3b70f74780f149dc622fe44c2755422c3ff
Mykola Konyk <mykola@sidefx.com>
2015-06-09 16:04:20 -0400

    Bumping to Houdini Version: 14.0.360 Houdini Engine Version: 1.9 (API: 20)

--- Source/HoudiniEngineRuntime/HoudiniEngineRuntime.Build.cs
+++ Source/HoudiniEngineRuntime/HoudiniEngineRuntime.Build.cs
@@ -16,7 +16,7 @@
 
 /*
 
-    Houdini Version: 14.0.359
+    Houdini Version: 14.0.360
     Houdini Engine Version: 1.9.20
     Unreal Version: 4.7.3
 
@@ -30,7 +30,7 @@ public class HoudiniEngineRuntime : ModuleRules
 	public HoudiniEngineRuntime( TargetInfo Target )
 	{
 		string HFSPath = "";
-		string HoudiniVersion = "14.0.359";
+		string HoudiniVersion = "14.0.360";
 
 		// Check if we are compiling on unsupported platforms.
 		if( Target.Platform != UnrealTargetPlatform.Win64 &&
@@ -107,22 +107,32 @@ public class HoudiniEngineRuntime : ModuleRules
 				"ShaderCore",
 				"InputCore",
 				"RHI",
-				"AssetTools",
-				"UnrealEd",
-				"Slate",
-				"SlateCore",
-				"Projects",
-				"PropertyEditor",
-				"ContentBrowser",
-				"RawMesh",
 				"TargetPlatform",
-				"LevelEditor",
-				"MainFrame",
-				"EditorStyle",
-				"EditorWidgets",
-				"AppFramework"
+				"RawMesh",
+				"Settings"
 			}
 		);
+
+		if (UEBuildConfiguration.bBuildEditor == true)
+		{
+			PublicDependencyModuleNames.AddRange(
+				new string[]
+				{
+					"AssetTools",
+					"UnrealEd",
+					"Slate",
+					"SlateCore",
+					"Projects",
+					"PropertyEditor",
+					"ContentBrowser",
+					"LevelEditor",
+					"MainFrame",
+					"EditorStyle",
+					"EditorWidgets",
+					"AppFramework"
+				}
+			);
+		}
 
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
