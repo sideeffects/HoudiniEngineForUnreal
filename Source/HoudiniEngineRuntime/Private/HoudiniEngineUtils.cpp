dafec2eeeb6493ddfe28c73c5f09be7473fb37e9
Mykola Konyk <mykola@sidefx.com>
2015-07-22 14:56:10 -0400

    Adding comments in texture generation code.

--- Source/HoudiniEngineRuntime/Private/HoudiniEngineUtils.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniEngineUtils.cpp
@@ -3435,6 +3435,7 @@ FHoudiniEngineUtils::HapiCreateMaterials(UHoudiniAssetComponent* HoudiniAssetCom
 							bCreatedNewTextureDiffuse = true;
 						}
 
+						// Reuse existing diffuse texture, or create new one.
 						TextureDiffuse = 
 								FHoudiniEngineUtils::CreateUnrealTexture(TextureDiffuse, ImageInfo, 
 									TextureDiffusePackage, TextureDiffuseName, PF_R8G8B8A8, ImageBuffer);
@@ -3497,6 +3498,7 @@ FHoudiniEngineUtils::HapiCreateMaterials(UHoudiniAssetComponent* HoudiniAssetCom
 								bCreatedNewTextureNormal = true;
 							}
 
+							// Reuse existing normal texture, or create new one.
 							TextureNormal = 
 									FHoudiniEngineUtils::CreateUnrealTexture(TextureNormal, ImageInfo, 
 										TextureNormalPackage, TextureNormalName, PF_R8G8B8A8, ImageBuffer, true);
