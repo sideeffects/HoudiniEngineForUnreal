c541b0ea64691440655bd56b00198f0ab4a5322a
Damian Campeanu <damian@sidefx.com>
2015-08-17 11:31:13 -0400

    Hiding temporary static meshes so the user is less likely to use them. I they want to use a temporary static mesh they should bake it via our baking feature.

--- Source/HoudiniEngineRuntime/Private/HoudiniEngineUtils.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniEngineUtils.cpp
@@ -1701,7 +1701,14 @@ FHoudiniEngineUtils::BakeCreateStaticMeshPackageForComponent(UHoudiniAssetCompon
 		else
 		{
 			// Create actual package.
-			PackageNew = CreatePackage(nullptr, *PackageName);
+			if(bBake)
+			{
+				PackageNew = CreatePackage(nullptr, *PackageName);
+			}
+			else
+			{
+				PackageNew = CreatePackage(HoudiniAssetComponent->GetOuter(), *PackageName);
+			}
 			break;
 		}
 	}
@@ -2475,7 +2482,7 @@ FHoudiniEngineUtils::CreateStaticMeshesFromHoudiniAsset(
 							FHoudiniEngineUtils::BakeCreateStaticMeshPackageForComponent(HoudiniAssetComponent,
 								HoudiniGeoPartObject, MeshName, MeshGuid);
 
-						StaticMesh = NewObject<UStaticMesh>(MeshPackage, FName(*MeshName), RF_Standalone | RF_Public);
+						StaticMesh = NewObject<UStaticMesh>(MeshPackage, FName(*MeshName), RF_Standalone);
 
 						// Add meta information to this package.
 						FHoudiniEngineUtils::AddHoudiniMetaInformationToPackage(MeshPackage, MeshPackage, 
