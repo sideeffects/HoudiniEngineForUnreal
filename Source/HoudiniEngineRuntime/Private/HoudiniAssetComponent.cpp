e0c16328bdb983701a3b3bb9fad139aed40c07a5
Mykola Konyk <mykola@sidefx.com>
2015-09-17 15:43:40 -0400

    Fixing a bug when Houdini assets which were marked as static, were not assigning static flag to generated static mesh components upon level loading.

--- Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
@@ -2100,6 +2100,16 @@ UHoudiniAssetComponent::PostLoad()
 	// Perform post load initialization on instance inputs.
 	PostLoadInitializeInstanceInputs();
 
+	// Update static mobility.
+	if(EComponentMobility::Static == Mobility)
+	{
+		for(TArray<USceneComponent*>::TIterator Iter(AttachChildren); Iter; ++Iter)
+		{
+			USceneComponent* SceneComponent = *Iter;
+			SceneComponent->SetMobility(EComponentMobility::Static);
+		}
+	}
+
 	// Need to update rendering information.
 	UpdateRenderingInformation();
 
