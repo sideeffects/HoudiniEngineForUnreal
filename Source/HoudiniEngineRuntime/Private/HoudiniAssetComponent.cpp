0b68af47163bbaec0c23fd4a935a1f09d26d225e
Mykola Konyk <mykola@sidefx.com>
2015-09-23 14:41:47 -0400

    Setting movable flags according to the parent Houdini component.

--- Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
@@ -537,6 +537,7 @@ UHoudiniAssetComponent::CreateObjectGeoPartResources(TMap<FHoudiniGeoPartObject,
 
 				StaticMeshComponent->SetStaticMesh(StaticMesh);
 				StaticMeshComponent->SetVisibility(true);
+				StaticMeshComponent->SetMobility(Mobility);
 				StaticMeshComponent->RegisterComponent();
 			}
 
