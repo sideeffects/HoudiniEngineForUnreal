18130658b8fafc90772f8cd6972a8270117bf52c
Mykola Konyk <mykola@sidefx.com>
2015-12-17 15:03:39 -0500

    Making sure RHI resources are released when we are rebuilding static meshes.

--- Source/HoudiniEngineRuntime/Private/HoudiniEngineUtils.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniEngineUtils.cpp
@@ -2102,6 +2102,9 @@ FHoudiniEngineUtils::CreateStaticMeshesFromHoudiniAsset(
 		return false;
 	}
 
+	// Make sure rendering is done - so we are not changing data being used by collision drawing.
+	FlushRenderingCommands();
+
 	// Get runtime settings.
 	const UHoudiniRuntimeSettings* HoudiniRuntimeSettings = GetDefault<UHoudiniRuntimeSettings>();
 	check(HoudiniRuntimeSettings);
@@ -3397,7 +3400,9 @@ FHoudiniEngineUtils::CreateStaticMeshesFromHoudiniAsset(
 						BodySetup->CollisionTraceFlag = ECollisionTraceFlag::CTF_UseComplexAsSimple;
 					}
 
-					//StaticMesh->PreEditChange(nullptr);
+					// Free any RHI resources.
+					StaticMesh->PreEditChange(nullptr);
+
 					FHoudiniScopedGlobalSilence ScopedGlobalSilence;
 					StaticMesh->Build(true);
 					StaticMesh->MarkPackageDirty();
