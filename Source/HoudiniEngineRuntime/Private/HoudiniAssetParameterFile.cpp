98751ec670d52a86b11a568d08b9758d363ece61
Mykola Konyk <mykola@sidefx.com>
2016-01-15 15:24:36 -0500

    Initial work on file parameter support.

---
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetParameterFile.cpp
@@ -0,0 +1,200 @@
+/*
+ * PROPRIETARY INFORMATION.  This software is proprietary to
+ * Side Effects Software Inc., and is not to be reproduced,
+ * transmitted, or disclosed in any way without written permission.
+ *
+ * Produced by:
+ *      Mykola Konyk
+ *      Side Effects Software Inc
+ *      123 Front Street West, Suite 1401
+ *      Toronto, Ontario
+ *      Canada   M5J 2M2
+ *      416-504-9876
+ *
+ */
+
+#include "HoudiniEngineRuntimePrivatePCH.h"
+#include "HoudiniEngineUtils.h"
+#include "HoudiniAssetParameterFile.h"
+#include "HoudiniAssetComponent.h"
+#include "HoudiniEngine.h"
+#include "HoudiniApi.h"
+
+
+UHoudiniAssetParameterFile::UHoudiniAssetParameterFile(const FObjectInitializer& ObjectInitializer) :
+	Super(ObjectInitializer)
+{
+	Values.Add(TEXT(""));
+}
+
+
+UHoudiniAssetParameterFile::~UHoudiniAssetParameterFile()
+{
+
+}
+
+
+UHoudiniAssetParameterFile*
+UHoudiniAssetParameterFile::Create(UHoudiniAssetComponent* InHoudiniAssetComponent,
+	UHoudiniAssetParameter* InParentParameter, HAPI_NodeId InNodeId, const HAPI_ParmInfo& ParmInfo)
+{
+	UObject* Outer = InHoudiniAssetComponent;
+	if(!Outer)
+	{
+		Outer = InParentParameter;
+		if(!Outer)
+		{
+			// Must have either component or parent not null.
+			check(false);
+		}
+	}
+
+	UHoudiniAssetParameterFile* HoudiniAssetParameterFile = NewObject<UHoudiniAssetParameterFile>(Outer,
+		UHoudiniAssetParameterFile::StaticClass(), NAME_None, RF_Public | RF_Transactional);
+
+	HoudiniAssetParameterFile->CreateParameter(InHoudiniAssetComponent, InParentParameter, InNodeId, ParmInfo);
+	return HoudiniAssetParameterFile;
+}
+
+
+bool
+UHoudiniAssetParameterFile::CreateParameter(UHoudiniAssetComponent* InHoudiniAssetComponent,
+	UHoudiniAssetParameter* InParentParameter, HAPI_NodeId InNodeId, const HAPI_ParmInfo& ParmInfo)
+{
+	if(!Super::CreateParameter(InHoudiniAssetComponent, InParentParameter, InNodeId, ParmInfo))
+	{
+		return false;
+	}
+
+	// We can only handle file type.
+	if(HAPI_PARMTYPE_FILE != ParmInfo.type)
+	{
+		return false;
+	}
+
+	// Assign internal Hapi values index.
+	SetValuesIndex(ParmInfo.stringValuesIndex);
+
+	// Get the actual value for this property.
+	TArray<HAPI_StringHandle> StringHandles;
+	StringHandles.SetNum(TupleSize);
+	if(HAPI_RESULT_SUCCESS != FHoudiniApi::GetParmStringValues(
+		FHoudiniEngine::Get().GetSession(), InNodeId, false, &StringHandles[0], ValuesIndex, TupleSize))
+	{
+		return false;
+	}
+
+	// Convert HAPI string handles to Unreal strings.
+	Values.SetNum(TupleSize);
+	for(int32 Idx = 0; Idx < TupleSize; ++Idx)
+	{
+		FString ValueString = TEXT("");
+		FHoudiniEngineUtils::GetHoudiniString(StringHandles[Idx], ValueString);
+		Values[Idx] = ValueString;
+	}
+
+	return true;
+}
+
+
+void
+UHoudiniAssetParameterFile::Serialize(FArchive& Ar)
+{
+	// Call base implementation.
+	Super::Serialize(Ar);
+
+	Ar << Values;
+}
+
+
+#if WITH_EDITOR
+
+void
+UHoudiniAssetParameterFile::CreateWidget(IDetailCategoryBuilder& DetailCategoryBuilder)
+{
+	Super::CreateWidget(DetailCategoryBuilder);
+
+	FDetailWidgetRow& Row = DetailCategoryBuilder.AddCustomRow(FText::GetEmpty());
+
+	// Create the standard parameter name widget.
+	CreateNameWidget(Row, true);
+
+	TSharedRef<SVerticalBox> VerticalBox = SNew(SVerticalBox);
+
+	for(int32 Idx = 0; Idx < TupleSize; ++Idx)
+	{
+		TSharedPtr<SEditableTextBox> EditableTextBox;
+
+		VerticalBox->AddSlot().Padding(2, 2, 5, 2)
+		[
+			SAssignNew(EditableTextBox, SEditableTextBox)
+			.Font(FEditorStyle::GetFontStyle(TEXT("PropertyWindow.NormalFont")))
+
+			.Text(FText::FromString(Values[Idx]))
+			.OnTextChanged(FOnTextChanged::CreateUObject(this, &UHoudiniAssetParameterFile::SetValue, Idx))
+			.OnTextCommitted(FOnTextCommitted::CreateUObject(this,
+				&UHoudiniAssetParameterFile::SetValueCommitted, Idx))
+		];
+
+		if(EditableTextBox.IsValid())
+		{
+			EditableTextBox->SetEnabled(!bIsDisabled);
+		}
+	}
+
+	Row.ValueWidget.Widget = VerticalBox;
+	Row.ValueWidget.MinDesiredWidth(HAPI_UNREAL_DESIRED_ROW_VALUE_WIDGET_WIDTH);
+}
+
+#endif
+
+
+bool
+UHoudiniAssetParameterFile::UploadParameterValue()
+{
+	for(int32 Idx = 0; Idx < Values.Num(); ++Idx)
+	{
+		std::string ConvertedString = TCHAR_TO_UTF8(*(Values[Idx]));
+		if(HAPI_RESULT_SUCCESS != FHoudiniApi::SetParmStringValue(
+			FHoudiniEngine::Get().GetSession(), NodeId, ConvertedString.c_str(), ParmId, Idx))
+		{
+			return false;
+		}
+	}
+
+	return Super::UploadParameterValue();
+}
+
+
+#if WITH_EDITOR
+
+void
+UHoudiniAssetParameterFile::SetValue(const FText& InValue, int32 Idx)
+{
+
+}
+
+
+void
+UHoudiniAssetParameterFile::SetValueCommitted(const FText& InValue, ETextCommit::Type CommitType, int32 Idx)
+{
+	FString CommittedValue = InValue.ToString();
+
+	if(Values[Idx] != CommittedValue)
+	{
+		// Record undo information.
+		FScopedTransaction Transaction(TEXT(HOUDINI_MODULE_RUNTIME),
+			LOCTEXT("HoudiniAssetParameterFileChange", "Houdini Parameter File: Changing a value"),
+			HoudiniAssetComponent);
+		Modify();
+
+		MarkPreChanged();
+
+		Values[Idx] = CommittedValue;
+
+		// Mark this parameter as changed.
+		MarkChanged();
+	}
+}
+
+#endif
---
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetParameterFile.h
@@ -0,0 +1,74 @@
+/*
+ * PROPRIETARY INFORMATION.  This software is proprietary to
+ * Side Effects Software Inc., and is not to be reproduced,
+ * transmitted, or disclosed in any way without written permission.
+ *
+ * Produced by:
+ *      Mykola Konyk
+ *      Side Effects Software Inc
+ *      123 Front Street West, Suite 1401
+ *      Toronto, Ontario
+ *      Canada   M5J 2M2
+ *      416-504-9876
+ *
+ */
+
+#pragma once
+#include "HoudiniAssetParameter.h"
+#include "HoudiniAssetParameterFile.generated.h"
+
+
+UCLASS()
+class HOUDINIENGINERUNTIME_API HoudiniAssetParameterFile : public UHoudiniAssetParameter
+{
+	GENERATED_UCLASS_BODY()
+
+public:
+
+	/** Destructor. **/
+	virtual ~HoudiniAssetParameterFile();
+
+public:
+
+	/** Create instance of this class. **/
+	static HoudiniAssetParameterFile* Create(UHoudiniAssetComponent* InHoudiniAssetComponent,
+		UHoudiniAssetParameter* InParentParameter, HAPI_NodeId InNodeId, const HAPI_ParmInfo& ParmInfo);
+
+public:
+
+	/** Create this parameter from HAPI information. **/
+	virtual bool CreateParameter(UHoudiniAssetComponent* InHoudiniAssetComponent,
+		UHoudiniAssetParameter* InParentParameter, HAPI_NodeId InNodeId, const HAPI_ParmInfo& ParmInfo) override;
+
+#if WITH_EDITOR
+
+	/** Create widget for this parameter and add it to a given category. **/
+	virtual void CreateWidget(IDetailCategoryBuilder& DetailCategoryBuilder) override;
+
+#endif
+
+	/** Upload parameter value to HAPI. **/
+	virtual bool UploadParameterValue() override;
+
+/** UObject methods. **/
+public:
+
+	virtual void Serialize(FArchive& Ar) override;
+
+public:
+
+#if WITH_EDITOR
+
+	/** Set value of this property, used by Slate. **/
+	void SetValue(const FText& InValue, int32 Idx);
+
+	/** Set value of this property through commit action, used by Slate. **/
+	void SetValueCommitted(const FText& InValue, ETextCommit::Type CommitType, int32 Idx);
+
+#endif
+
+protected:
+
+	/** Values of this property. **/
+	TArray<FString> Values;
+};
