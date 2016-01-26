7ef500ef7be4192cc6181fd59573c27ac787c5f4
Mykola Konyk <mykola@sidefx.com>
2016-01-26 13:57:56 -0500

    Adding localization header files.

---
+++ Source/HoudiniEngineEditor/Private/HoudiniEngineEditorLocalization.h
@@ -0,0 +1,16 @@
+/*
+ * PROPRIETARY INFORMATION.  This software is proprietary to
+ * Side Effects Software Inc., and is not to be reproduced,
+ * transmitted, or disclosed in any way without written permission.
+ *
+ * Produced by:
+ *      Mykola Konyk
+ *      Side Effects Software Inc
+ *      123 Front Street West, Suite 1401
+ *      Toronto, Ontario
+ *      Canada   M5J 2M2
+ *      416-504-9876
+ *
+ */
+
+#pragma once
