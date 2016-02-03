a5abfca736acc9e1136c9bf1e0953585536894fd
Mykola Konyk <mykola@sidefx.com>
2016-02-03 16:48:18 -0500

    Adding support for roughness expressions. Will look for ogl_roughmap or rough_texture parameters first; if not found, will attempt to look for float values ogl_rough and rough parameters.

--- Source/HoudiniEngineRuntime/Private/HoudiniEngineRuntimePrivatePCH.h
+++ Source/HoudiniEngineRuntime/Private/HoudiniEngineRuntimePrivatePCH.h
@@ -76,6 +76,7 @@
 #include "Materials/MaterialExpressionTextureSample.h"
 #include "Materials/MaterialExpressionTextureCoordinate.h"
 #include "Materials/MaterialExpressionConstant4Vector.h"
+#include "Materials/MaterialExpressionConstant.h"
 #include "StaticMeshResources.h"
 #include "ISettingsModule.h"
 #include "TargetPlatform.h"
