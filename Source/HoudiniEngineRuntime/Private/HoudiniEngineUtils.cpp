063fe4f3f01d564daf78a21cf26b701d83838cd2
Mykola Konyk <mykola@sidefx.com>
2016-02-07 16:32:47 -0500

    Storing instancer material within the GeoObjectPart.

--- Source/HoudiniEngineRuntime/Private/HoudiniEngineUtils.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniEngineUtils.cpp
@@ -2678,6 +2678,22 @@ FHoudiniEngineUtils::CreateStaticMeshesFromHoudiniAsset(
 				{
 					if(PartInfo.pointCount > 0)
 					{
+						// We need to check whether this instancer has a material.
+						HAPI_MaterialId const* FoundInstancerMaterialId = 
+							InstancerMaterialMap.Find(HoudiniGeoPartObject);
+						if(FoundInstancerMaterialId)
+						{
+							HAPI_MaterialId InstancerMaterialId = *FoundInstancerMaterialId;
+
+							FString InstancerMaterialShopName = TEXT("");
+							if(FHoudiniEngineUtils::GetUniqueMaterialShopName(AssetId, InstancerMaterialId,
+								InstancerMaterialShopName))
+							{
+								HoudiniGeoPartObject.bInstancerMaterialAvailable = true;
+								HoudiniGeoPartObject.InstancerMaterialName = InstancerMaterialShopName;
+							}
+						}
+
 						// Instancer objects have no mesh assigned.
 						StaticMesh = nullptr;
 						StaticMeshesOut.Add(HoudiniGeoPartObject, StaticMesh);
@@ -4168,8 +4184,7 @@ FHoudiniEngineUtils::HapiCreateMaterials(UHoudiniAssetComponent* HoudiniAssetCom
 
 		if(MaterialInfo.exists)
 		{
-			FString MaterialShopName;
-
+			FString MaterialShopName = TEXT("");
 			if(!FHoudiniEngineUtils::GetUniqueMaterialShopName(AssetId, MaterialId, MaterialShopName))
 			{
 				continue;
@@ -6257,7 +6272,7 @@ FHoudiniEngineUtils::ExtractUniqueMaterialIds(const HAPI_AssetInfo& AssetInfo, T
 
 						if(-1 != InstanceMaterialId)
 						{
-							FHoudiniGeoPartObject GeoPartObject(ObjectInfo.id, GeoInfo.id, PartInfo.id, 0);
+							FHoudiniGeoPartObject GeoPartObject(AssetInfo.id, ObjectInfo.id, GeoInfo.id, PartInfo.id);
 							InstancerMaterialMap.Add(GeoPartObject, InstanceMaterialId);
 
 							InstancerMaterialIds.Add(InstanceMaterialId);
