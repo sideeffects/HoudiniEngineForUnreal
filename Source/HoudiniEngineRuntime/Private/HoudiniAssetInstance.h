676413f2a72714ffe66b88f6731589a751540061
Mykola Konyk <mykola@sidefx.com>
2016-03-28 16:39:52 -0400

    Work in progress on API.

---
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetInstance.h
@@ -0,0 +1,26 @@
+/*
+* PROPRIETARY INFORMATION.  This software is proprietary to
+* Side Effects Software Inc., and is not to be reproduced,
+* transmitted, or disclosed in any way without written permission.
+*
+* Represents a Houdini Asset in the UE4 Content Browser.
+*
+* Produced by:
+*      Mykola Konyk
+*      Side Effects Software Inc
+*      123 Front Street West, Suite 1401
+*      Toronto, Ontario
+*      Canada   M5J 2M2
+*      416-504-9876
+*
+*/
+
+#pragma once
+#include "HoudiniAssetInstance.generated.h"
+
+
+UCLASS(EditInlineNew, config=Engine)
+class HOUDINIENGINERUNTIME_API UHoudiniAssetInstance : public UObject
+{
+	GENERATED_UCLASS_BODY()
+};
