dc4069c8daa6ebccffeb90ba57e759c5f8909406
Christopher  Grebeldinger <chrisg@sidefx.com>
2016-10-06 16:51:47 -0400

    Bumping to Houdini Version: 16.0.390 Houdini Engine Version: 3.0 (API: 43)

--- Source/HoudiniEngineRuntime/Public/HAPI/HAPI_Version.h
+++ Source/HoudiniEngineRuntime/Public/HAPI/HAPI_Version.h
@@ -29,7 +29,7 @@
 // expecting to compile against.
 #define HAPI_VERSION_HOUDINI_MAJOR 16
 #define HAPI_VERSION_HOUDINI_MINOR 0
-#define HAPI_VERSION_HOUDINI_BUILD 389
+#define HAPI_VERSION_HOUDINI_BUILD 390
 #define HAPI_VERSION_HOUDINI_PATCH 0
 
 // The two components of the Houdini Engine (marketed) version.
