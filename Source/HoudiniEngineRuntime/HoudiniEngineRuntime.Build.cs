62b5a989267982ecf72337b15725e4bc5c1505d2
Damien Pernuit <damienp@sidefx.com>
2016-12-21 14:58:28 -0500

    Bumping to Houdini Version: 15.5.695 Houdini Engine Version: 2.0 (API: 24)

--- Source/HoudiniEngineRuntime/HoudiniEngineRuntime.Build.cs
+++ Source/HoudiniEngineRuntime/HoudiniEngineRuntime.Build.cs
@@ -16,7 +16,7 @@
 
 /*
 
-    Houdini Version: 15.5.694
+    Houdini Version: 15.5.695
     Houdini Engine Version: 2.0.24
     Unreal Version: 4.14.0
 
@@ -31,7 +31,7 @@ public class HoudiniEngineRuntime : ModuleRules
 	{
 		bool bIsRelease = true;
 		string HFSPath = "";
-		string HoudiniVersion = "15.5.694";
+		string HoudiniVersion = "15.5.695";
 
 		// Check if we are compiling on unsupported platforms.
 		if( Target.Platform != UnrealTargetPlatform.Win64 &&
