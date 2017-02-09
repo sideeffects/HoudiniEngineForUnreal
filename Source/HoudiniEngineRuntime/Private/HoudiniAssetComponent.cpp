1f5aca2d9232de79a68f0566c6f6ef0efe534529
Christopher  Grebeldinger <chrisg@sidefx.com>
2017-02-09 15:41:53 -0500

    Fix check for invisible parent folders

--- Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
@@ -773,6 +773,9 @@ UHoudiniAssetComponent::CreateObjectGeoPartResources( TMap< FHoudiniGeoPartObjec
 
                     FHoudiniEngineUtils::AddActorsToMeshSocket( StaticMesh->Sockets[nSocket], StaticMeshComponent );
                 }
+
+                // Try to update uproperty atributes
+                // FHoudiniEngineUtils::UpdateUPropertyAttributes(StaticMeshComponent, HoudiniGeoPartObject);
             }
         }
     }
@@ -3519,15 +3522,19 @@ UHoudiniAssetComponent::CreateParameters()
             if ( ParmInfo.invisible )
                 continue;
 
-            // Check if any parent of this parameter is invisible
+            // Check if any parent folder of this parameter is invisible
             bool ParentInvisible = false;
-            HAPI_ParmId ParentIdx = ParmInfo.parentId;
-            while (ParentIdx > 0 && !ParentInvisible)
+            HAPI_ParmId ParentId = ParmInfo.parentId;
+            while (ParentId > 0 && !ParentInvisible)
             {
-                if (ParmInfos[ParentIdx].invisible)
-                    ParentInvisible = true;
-                else
-                    ParentIdx = ParmInfos[ParentIdx].parentId;
+                if( const HAPI_ParmInfo* ParentInfoPtr = ParmInfos.FindByPredicate( [=]( const HAPI_ParmInfo& Info ) {
+                    return Info.id == ParentId;
+                } ) )
+                {
+                    if( ParentInfoPtr->invisible && ParentInfoPtr->type == HAPI_PARMTYPE_FOLDER )
+                        ParentInvisible = true;
+                    ParentId = ParentInfoPtr->parentId;
+                }
             }
 
             if ( ParentInvisible )
