26215a1005af35e8ae174f51619b8c783795fe62
Christopher  Grebeldinger <chrisg@sidefx.com>
2017-02-22 11:18:35 -0500

    Make intermediate UStaticMesh non-transactional and remove RT crash workaround in HAC PreEditUndo. There is no need to transact mesh change because a re-cook is ultimately required anyway.

--- Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.h
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.h
@@ -360,7 +360,6 @@ public:
 #if WITH_EDITOR
 
         virtual void PostEditChangeProperty( FPropertyChangedEvent & PropertyChangedEvent ) override;
-        virtual void PreEditUndo() override;
         virtual void PostEditUndo() override;
         virtual void PostEditImport() override;
 #endif
