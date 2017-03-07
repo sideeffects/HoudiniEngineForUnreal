25cd1209245836835e94bc4fe0c71d0e464c13e1
Damien Pernuit <damienp@sidefx.com>
2017-03-07 18:01:34 -0500

    fixed simple colliders (box, sphere etc.) creating additional static meshes even when not rendered collision geos. fix possible crash with asset that generates Landscapes. fixed build error.

--- Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
@@ -4919,8 +4919,8 @@ UHoudiniAssetComponent::CreateLandscape(
     if ( FMath::Abs(LandscapePosition.Z) < SMALL_NUMBER )
     {
         // XZ orientation, we just need to invert Y
-        LandscapePosition.Y = -LandscapePosition.Y;
-        LandscapePosition.Z = 0.0f;
+        //LandscapePosition.Y = -LandscapePosition.Y;
+        //LandscapePosition.Z = 0.0f;
     }
     else if ( FMath::Abs( LandscapePosition.X ) < SMALL_NUMBER )
     {
@@ -4946,7 +4946,8 @@ UHoudiniAssetComponent::CreateLandscape(
     uint16 HoudiniZeroValueInDigit = FMath::RoundToInt( -(double)FloatMin * ZSpacing + ZCenterOffset );
 
     float ZOffset = -( (float)HoudiniZeroValueInDigit - 32768.0f ) / 128.0f * LandscapeScale.Z;
-    LandscapePosition.Z = ZOffset;
+    //LandscapePosition.Z += ZOffset;
+    LandscapePosition.Z += ZOffset;
 
     // We can now set the Landscape position
     Landscape->SetActorRelativeLocation( LandscapePosition );
@@ -5193,6 +5194,8 @@ UHoudiniAssetComponent::ClearLandscapes()
     for (TMap< FHoudiniGeoPartObject, ALandscape * >::TIterator Iter(LandscapeComponents); Iter; ++Iter)
     {
         ALandscape * HoudiniLandscape = Iter.Value();
+        if ( !HoudiniLandscape )
+            continue;
 
         //HoudiniLandscape->DetachFromComponent( FDetachmentTransformRules::KeepRelativeTransform );
         HoudiniLandscape->UnregisterAllComponents();
