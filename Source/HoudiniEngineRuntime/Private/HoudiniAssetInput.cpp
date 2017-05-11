09a48c7b050001e26e8bc508feb0af16d9bf5fd9
Damien Pernuit <damienp@sidefx.com>
2017-05-11 15:31:44 -0400

    fix development build error.

--- Source/HoudiniEngineRuntime/Private/HoudiniAssetInput.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetInput.cpp
@@ -1980,7 +1980,6 @@ UHoudiniAssetInput::OnChoiceChange( TSharedPtr< FString > NewChoice, ESelectInfo
     ChangeInputType(newChoice);
 }
 
-
 bool
 UHoudiniAssetInput::ChangeInputType(const EHoudiniAssetInputType::Enum& newType)
 {
@@ -3589,6 +3588,7 @@ UHoudiniAssetInput::GetInputBounds()
 
 void UHoudiniAssetInput::SetDefaultInputTypeFromLabel()
 {
+#if WITH_EDITOR
     // Look if we can find an input type prefix in the input name
     FString inputName = GetParameterLabel();
 
@@ -3604,7 +3604,7 @@ void UHoudiniAssetInput::SetDefaultInputTypeFromLabel()
     FString assetPrefix2	= TEXT( "hda" );
 
     if ( inputName.Contains( curvePrefix, ESearchCase::IgnoreCase ) )
-        ChangeInputType(EHoudiniAssetInputType::CurveInput);
+        ChangeInputType( EHoudiniAssetInputType::CurveInput );
 
     else if ( ( inputName.Contains( landscapePrefix, ESearchCase::IgnoreCase ) ) 
             || ( inputName.Contains( landscapePrefix2, ESearchCase::IgnoreCase ) )
@@ -3624,6 +3624,9 @@ void UHoudiniAssetInput::SetDefaultInputTypeFromLabel()
         // By default, geometry input is chosen.
         ChoiceIndex = EHoudiniAssetInputType::GeometryInput;
     }
+#else
+    ChoiceIndex = EHoudiniAssetInputType::GeometryInput;
+#endif
 }
 
 #undef LOCTEXT_NAMESPACE
