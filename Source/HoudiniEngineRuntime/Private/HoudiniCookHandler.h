266743cceca27e8ac4db9c6992762f00a6c3c0b7
Damien Pernuit <damienp@sidefx.com>
2017-05-17 16:12:30 -0400

    fixed regressions making all bake action not properly baking materials and texture: Materials and textures are now properly baked to the bake folder instead of the temp folder.

--- Source/HoudiniEngineRuntime/Private/HoudiniCookHandler.h
+++ Source/HoudiniEngineRuntime/Private/HoudiniCookHandler.h
@@ -26,29 +26,43 @@ public:
 
 struct FHoudiniCookParams
 {
-    class UHoudiniAsset* HoudiniAsset = nullptr;
-    /** object that handles material and mesh caches as they are built */
-    IHoudiniCookHandler* HoudiniCookManager = nullptr;
-    EBakeMode StaticMeshBakeMode = EBakeMode::Intermediate;
-    EBakeMode MaterialAndTextureBakeMode = EBakeMode::CookToTemp;
-    /** GUID used to decorate package names in intermediate mode */
-    FGuid PackageGUID;
-    /** Transient cache of last baked parts */
-    TMap<FHoudiniGeoPartObject, TWeakObjectPtr<class UPackage> >* BakedStaticMeshPackagesForParts = nullptr;
-    /** Transient cache of last baked materials and textures */
-    TMap<FString, TWeakObjectPtr<class UPackage> >* BakedMaterialPackagesForIds = nullptr;
-    /** Cache of the temp cook content packages created by the asset for its materials/textures		    **/
-    TMap<FHoudiniGeoPartObject, TWeakObjectPtr<class UPackage> >* CookedTemporaryStaticMeshPackages = nullptr;
-    /** Cache of the temp cook content packages created by the asset for its materials/textures		    **/
-    TMap<FString, TWeakObjectPtr<class UPackage> >* CookedTemporaryPackages = nullptr;
-    /** When cooking in temp mode - folder to create assets in */
-    FText TempCookFolder;
-    /** When cooking in bake mode - folder to create assets in */
-    FText BakeFolder;
-    /** When cooking in intermediate mode - uobject to use as outer */
-    class UObject* IntermediateOuter = nullptr;
-    int32 GeneratedDistanceFieldResolutionScale = 0;
+    FHoudiniCookParams(class UHoudiniAsset* InHoudiniAsset);
+    FHoudiniCookParams(class UHoudiniAssetComponent* HoudiniAssetComponent);
 
-    FHoudiniCookParams( class UHoudiniAsset* InHoudiniAsset );
-    FHoudiniCookParams( class UHoudiniAssetComponent* HoudiniAssetComponent );
+    // Helper functions returning the default behavior expected when cooking mesh
+    static EBakeMode GetDefaultStaticMeshesCookMode() { return EBakeMode::Intermediate; };
+    // Helper functions returning the default behavior expected when cooking materials or textures
+    static EBakeMode GetDefaultMaterialAndTextureCookMode() { return EBakeMode::CookToTemp; };
+
+
+    // Members
+
+    // The attached HoudiniAsset
+    class UHoudiniAsset* HoudiniAsset = nullptr;
+
+    // Object that handles material and mesh caches as they are built
+    IHoudiniCookHandler* HoudiniCookManager = nullptr;
+    EBakeMode StaticMeshBakeMode = GetDefaultStaticMeshesCookMode();
+    EBakeMode MaterialAndTextureBakeMode = GetDefaultMaterialAndTextureCookMode();
+
+    // GUID used to decorate package names in intermediate mode
+    FGuid PackageGUID;
+
+    // Transient cache of last baked parts
+    TMap<FHoudiniGeoPartObject, TWeakObjectPtr<class UPackage> >* BakedStaticMeshPackagesForParts = nullptr;
+    // Transient cache of last baked materials and textures
+    TMap<FString, TWeakObjectPtr<class UPackage> >* BakedMaterialPackagesForIds = nullptr;
+    // Cache of the temp cook content packages created by the asset for its materials/textures
+    TMap<FHoudiniGeoPartObject, TWeakObjectPtr<class UPackage> >* CookedTemporaryStaticMeshPackages = nullptr;
+    // Cache of the temp cook content packages created by the asset for its materials/textures
+    TMap<FString, TWeakObjectPtr<class UPackage> >* CookedTemporaryPackages = nullptr;
+
+    // When cooking in temp mode - folder to create assets in
+    FText TempCookFolder;
+    // When cooking in bake mode - folder to create assets in
+    FText BakeFolder;
+    // When cooking in intermediate mode - uobject to use as outer
+    class UObject* IntermediateOuter = nullptr;
+
+    int32 GeneratedDistanceFieldResolutionScale = 0;
 };
