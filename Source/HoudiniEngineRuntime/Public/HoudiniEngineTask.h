0572ddde5b788e51efcbf83f432e2ec7cf2a5fe6
Christopher  Grebeldinger <chrisg@sidefx.com>
2017-05-31 11:59:48 -0400

    Move some basic api files into houdiniengineruntime public

--- Source/HoudiniEngineRuntime/Public/HoudiniEngineTask.h
+++ Source/HoudiniEngineRuntime/Public/HoudiniEngineTask.h
@@ -19,14 +19,6 @@
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 * SOFTWARE.
 *
-* Produced by:
-*      Mykola Konyk
-*      Side Effects Software Inc
-*      123 Front Street West, Suite 1401
-*      Toronto, Ontario
-*      Canada   M5J 2M2
-*      416-504-9876
-*
 */
 
 #pragma once
