c1fc3310abf51f952cdb495fa42146fda747055b
Christopher  Grebeldinger <chrisg@sidefx.com>
2017-08-08 10:19:50 -0400

    Move instance input param UI into editor

--- Source/HoudiniEngineRuntime/Private/HoudiniAssetParameterColor.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetParameterColor.cpp
@@ -33,7 +33,6 @@
 UHoudiniAssetParameterColor::UHoudiniAssetParameterColor( const FObjectInitializer & ObjectInitializer )
     : Super( ObjectInitializer )
     , Color( FColor::White )
-    , bIsColorPickerOpen( false )
 {}
 
 void
@@ -165,16 +164,6 @@ UHoudiniAssetParameterColor::GetColor() const
 {
     return Color;
 }
-
-#if WITH_EDITOR
-
-bool
-UHoudiniAssetParameterColor::IsColorPickerWindowOpen() const
-{
-    return bIsColorPickerOpen;
-}
-
-#endif // WITH_EDITOR
 
 void
 UHoudiniAssetParameterColor::OnPaintColorChanged( FLinearColor InNewColor, bool bTriggerModify, bool bRecordUndo )
