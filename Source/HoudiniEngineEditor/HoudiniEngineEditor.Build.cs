18f356d6f11707c05d806ac8d1e8247410f127d4
Damien Pernuit <damienp@sidefx.com>
2017-09-06 10:37:38 -0400

    Bumping to Houdini Version: 16.5.225 Houdini Engine Version: 3.1 (API: 5)

--- Source/HoudiniEngineEditor/HoudiniEngineEditor.Build.cs
+++ Source/HoudiniEngineEditor/HoudiniEngineEditor.Build.cs
@@ -32,7 +32,7 @@
 
 /*
 
-    Houdini Version: 16.5.218
+    Houdini Version: 16.5.225
     Houdini Engine Version: 3.1.5
     Unreal Version: 4.17.0
 
@@ -48,7 +48,7 @@ public class HoudiniEngineEditor : ModuleRules
 		PCHUsage = PCHUsageMode.UseSharedPCHs;
 		bool bIsRelease = true;
 		string HFSPath = "";
-		string HoudiniVersion = "16.5.218";
+		string HoudiniVersion = "16.5.225";
 
 		// Check if we are compiling on unsupported platforms.
 		if( Target.Platform != UnrealTargetPlatform.Win64 &&
