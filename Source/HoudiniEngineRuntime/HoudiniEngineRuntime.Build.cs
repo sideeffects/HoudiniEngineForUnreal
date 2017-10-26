a8c18a5cab6d93f6bb7810799782fb28c6efe7d0
Damien Pernuit <damienp@sidefx.com>
2017-10-26 17:11:59 -0400

    Bumping to Houdini Version: 16.0.772 Houdini Engine Version: 3.0 (API: 62)

--- Source/HoudiniEngineRuntime/HoudiniEngineRuntime.Build.cs
+++ Source/HoudiniEngineRuntime/HoudiniEngineRuntime.Build.cs
@@ -32,7 +32,7 @@
 
 /*
 
-    Houdini Version: 16.0.762
+    Houdini Version: 16.0.772
     Houdini Engine Version: 3.0.62
     Unreal Version: 4.17.0
 
@@ -49,7 +49,7 @@ public class HoudiniEngineRuntime : ModuleRules
 		PCHUsage = PCHUsageMode.UseSharedPCHs;
 		bool bIsRelease = true;
 		string HFSPath = "";
-		string HoudiniVersion = "16.0.762";
+		string HoudiniVersion = "16.0.772";
 		PlatformID platformId = Environment.OSVersion.Platform;
 
 		// Check if we are compiling on unsupported platforms.
