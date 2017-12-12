632bce27f10ff405514b1db2a625540a9575e2c8
Chris  Grebeldinger <chrisg@sidefx.com>
2017-12-12 16:46:18 -0500

    Fix placement mode dependency

--- Source/HoudiniEngineEditor/Private/HoudiniEngineEditor.cpp
+++ Source/HoudiniEngineEditor/Private/HoudiniEngineEditor.cpp
@@ -48,7 +48,7 @@
 #include "EditorDirectories.h"
 #include "Styling/SlateStyleRegistry.h"
 #include "SHoudiniToolPalette.h"
-#include "Editor/PlacementMode/Public/IPlacementModeModule.h"
+#include "IPlacementModeModule.h"
 
 #define LOCTEXT_NAMESPACE HOUDINI_LOCTEXT_NAMESPACE 
 
