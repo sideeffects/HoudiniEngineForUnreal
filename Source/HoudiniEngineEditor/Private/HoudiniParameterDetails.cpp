64befd331246eeb7030ed58e0c643d387e5ff7b4
Damien Pernuit <damienp@sidefx.com>
2018-05-17 17:40:25 -0400

    Fixed some issues with Ramp parameters UI. Fixed a crash when changing the current level with Ramp parameter UI still opened in the Details Panel. Fixed possible crashes with Instances.

--- Source/HoudiniEngineEditor/Private/HoudiniParameterDetails.cpp
+++ Source/HoudiniEngineEditor/Private/HoudiniParameterDetails.cpp
@@ -23,6 +23,7 @@
 
 #include "HoudiniApi.h"
 #include "HoudiniParameterDetails.h"
+#include "HoudiniAssetComponentDetails.h"
 #include "HoudiniEngineEditorPrivatePCH.h"
 
 #include "HoudiniAssetComponent.h"
@@ -468,49 +469,62 @@ public:
         , _ViewMaxInput( 10.0f )
         , _ViewMinOutput( 0.0f )
         , _ViewMaxOutput( 1.0f )
-        , _XAxisName()
-        , _YAxisName()
-        , _HideUI( true )
-        , _DrawCurve( true )
         , _InputSnap( 0.1f )
         , _OutputSnap( 0.05f )
-        , _SnappingEnabled( false )
+        , _InputSnappingEnabled( false )
+        , _OutputSnappingEnabled( false )
+        , _ShowTimeInFrames( false )
         , _TimelineLength( 5.0f )
         , _DesiredSize( FVector2D::ZeroVector )
+        , _DrawCurve( true )
+        , _HideUI( true )
         , _AllowZoomOutput( true )
         , _AlwaysDisplayColorCurves( false )
         , _ZoomToFitVertical( true )
         , _ZoomToFitHorizontal( true )
         , _ShowZoomButtons( true )
+        , _XAxisName()
+        , _YAxisName()
         , _ShowInputGridNumbers( true )
         , _ShowOutputGridNumbers( true )
         , _ShowCurveSelector( true )
         , _GridColor( FLinearColor( 0.0f, 0.0f, 0.0f, 0.3f ) )
-    {}
+        {
+            _Clipping = EWidgetClipping::ClipToBounds;
+        }
 
-    SLATE_ATTRIBUTE( float, ViewMinInput )
-    SLATE_ATTRIBUTE( float, ViewMaxInput )
-    SLATE_ATTRIBUTE( float, ViewMinOutput )
-    SLATE_ATTRIBUTE( float, ViewMaxOutput )
-    SLATE_ARGUMENT( TOptional< FString >, XAxisName )
-    SLATE_ARGUMENT( TOptional< FString >, YAxisName )
-    SLATE_ARGUMENT( bool, HideUI )
-    SLATE_ARGUMENT( bool, DrawCurve )
-    SLATE_ATTRIBUTE( float, InputSnap )
-    SLATE_ATTRIBUTE( float, OutputSnap )
-    SLATE_ATTRIBUTE( bool, SnappingEnabled )
-    SLATE_ATTRIBUTE( float, TimelineLength )
-    SLATE_ATTRIBUTE( FVector2D, DesiredSize )
-    SLATE_ARGUMENT( bool, AllowZoomOutput )
-    SLATE_ARGUMENT( bool, AlwaysDisplayColorCurves )
-    SLATE_ARGUMENT( bool, ZoomToFitVertical )
-    SLATE_ARGUMENT( bool, ZoomToFitHorizontal )
-    SLATE_ARGUMENT( bool, ShowZoomButtons )
-    SLATE_ARGUMENT( bool, ShowInputGridNumbers )
-    SLATE_ARGUMENT( bool, ShowOutputGridNumbers )
-    SLATE_ARGUMENT( bool, ShowCurveSelector )
-    SLATE_ARGUMENT( FLinearColor, GridColor )
-    SLATE_END_ARGS()
+    SLATE_ATTRIBUTE(float, ViewMinInput)
+        SLATE_ATTRIBUTE(float, ViewMaxInput)
+        SLATE_ATTRIBUTE(TOptional<float>, DataMinInput)
+        SLATE_ATTRIBUTE(TOptional<float>, DataMaxInput)
+        SLATE_ATTRIBUTE(float, ViewMinOutput)
+        SLATE_ATTRIBUTE(float, ViewMaxOutput)
+        SLATE_ATTRIBUTE(float, InputSnap)
+        SLATE_ATTRIBUTE(float, OutputSnap)
+        SLATE_ATTRIBUTE(bool, InputSnappingEnabled)
+        SLATE_ATTRIBUTE(bool, OutputSnappingEnabled)
+        SLATE_ATTRIBUTE(bool, ShowTimeInFrames)
+        SLATE_ATTRIBUTE(float, TimelineLength)
+        SLATE_ATTRIBUTE(FVector2D, DesiredSize)
+        SLATE_ATTRIBUTE(bool, AreCurvesVisible)
+        SLATE_ARGUMENT(bool, DrawCurve)
+        SLATE_ARGUMENT(bool, HideUI)
+        SLATE_ARGUMENT(bool, AllowZoomOutput)
+        SLATE_ARGUMENT(bool, AlwaysDisplayColorCurves)
+        SLATE_ARGUMENT(bool, ZoomToFitVertical)
+        SLATE_ARGUMENT(bool, ZoomToFitHorizontal)
+        SLATE_ARGUMENT(bool, ShowZoomButtons)
+        SLATE_ARGUMENT(TOptional<FString>, XAxisName)
+        SLATE_ARGUMENT(TOptional<FString>, YAxisName)
+        SLATE_ARGUMENT(bool, ShowInputGridNumbers)
+        SLATE_ARGUMENT(bool, ShowOutputGridNumbers)
+        SLATE_ARGUMENT(bool, ShowCurveSelector)
+        SLATE_ARGUMENT(FLinearColor, GridColor)
+        SLATE_EVENT( FOnSetInputViewRange, OnSetInputViewRange )
+        SLATE_EVENT( FOnSetOutputViewRange, OnSetOutputViewRange )
+        SLATE_EVENT( FOnSetAreCurvesVisible, OnSetAreCurvesVisible )
+        SLATE_EVENT( FSimpleDelegate, OnCreateAsset )
+        SLATE_END_ARGS()
 
 public:
 
@@ -588,8 +602,6 @@ FHoudiniParameterDetails::CreateWidgetRamp( IDetailCategoryBuilder & LocalDetail
     
     // Create the standard parameter name widget.
     CreateNameWidget( &InParam, Row, true );
-    
-    TSharedPtr<SHoudiniAssetParameterRampCurveEditor> CurveEditor;
 
     TSharedRef< SHorizontalBox > HorizontalBox = SNew( SHorizontalBox );
 
@@ -615,7 +627,7 @@ FHoudiniParameterDetails::CreateWidgetRamp( IDetailCategoryBuilder & LocalDetail
         SNew( SBorder )
         .VAlign( VAlign_Fill )
         [
-            SAssignNew( CurveEditor, SHoudiniAssetParameterRampCurveEditor )
+            SAssignNew( InParam.CurveEditor, SHoudiniAssetParameterRampCurveEditor )
             .ViewMinInput( 0.0f )
             .ViewMaxInput( 1.0f )
             .HideUI( true )
@@ -638,8 +650,9 @@ FHoudiniParameterDetails::CreateWidgetRamp( IDetailCategoryBuilder & LocalDetail
     ];
 
     // Set callback for curve editor events.
-    if ( CurveEditor.IsValid() )
-        CurveEditor->SetParentRampParameter( &InParam );
+    TSharedPtr< SHoudiniAssetParameterRampCurveEditor > HoudiniRampEditor = StaticCastSharedPtr<SHoudiniAssetParameterRampCurveEditor>(InParam.CurveEditor );
+    if ( HoudiniRampEditor.IsValid() )
+        HoudiniRampEditor->SetParentRampParameter( &InParam );
 
     if ( InParam.bIsFloatRamp )
     {
@@ -657,7 +670,7 @@ FHoudiniParameterDetails::CreateWidgetRamp( IDetailCategoryBuilder & LocalDetail
         InParam.GenerateCurvePoints();
 
         // Set the curve that is being edited.
-        CurveEditor->SetCurveOwner( InParam.HoudiniAssetParameterRampCurveFloat, true );
+        InParam.CurveEditor->SetCurveOwner( InParam.HoudiniAssetParameterRampCurveFloat, true );
     }
     else
     {
@@ -675,7 +688,7 @@ FHoudiniParameterDetails::CreateWidgetRamp( IDetailCategoryBuilder & LocalDetail
         InParam.GenerateCurvePoints();
 
         // Set the curve that is being edited.
-        CurveEditor->SetCurveOwner( InParam.HoudiniAssetParameterRampCurveColor, true );
+        InParam.CurveEditor->SetCurveOwner( InParam.HoudiniAssetParameterRampCurveColor, true );
     }
 
     Row.ValueWidget.Widget = HorizontalBox;
