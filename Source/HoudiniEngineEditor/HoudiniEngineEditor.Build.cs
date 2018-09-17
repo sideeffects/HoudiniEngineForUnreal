d7558b0a2f695a5f66e0862d54cb4a65d03f9c6a
Damien Pernuit <damienp@sidefx.com>
2018-09-17 12:54:44 -0400

    Bumping to Houdini Version: 17.4.19 Houdini Engine Version: 3.2 (API: 26)

--- Source/HoudiniEngineEditor/HoudiniEngineEditor.Build.cs
+++ Source/HoudiniEngineEditor/HoudiniEngineEditor.Build.cs
@@ -32,8 +32,8 @@
 
 /*
 
-    Houdini Version: 17.4.15
-    Houdini Engine Version: 3.2.25
+    Houdini Version: 17.4.19
+    Houdini Engine Version: 3.2.26
     Unreal Version: 4.20.2
 
 */
@@ -46,7 +46,7 @@ public class HoudiniEngineEditor : ModuleRules
 {
     private string GetHFSPath()
     {
-        string HoudiniVersion = "17.4.15";
+        string HoudiniVersion = "17.4.19";
         bool bIsRelease = true;
         string HFSPath = "";
         string RegistryPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Side Effects Software";
