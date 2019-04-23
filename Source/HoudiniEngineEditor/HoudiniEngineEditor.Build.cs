43db7ab38bba86f933250a2eb2c807608e21856a
Damien Pernuit <damienp@sidefx.com>
2019-04-23 17:36:54 -0400

    Bumping to Houdini Version: 17.5.236 Houdini Engine Version: 3.2 (API: 40)

--- Source/HoudiniEngineEditor/HoudiniEngineEditor.Build.cs
+++ Source/HoudiniEngineEditor/HoudiniEngineEditor.Build.cs
@@ -32,7 +32,7 @@
 
 /*
 
-    Houdini Version: 17.5.231
+    Houdini Version: 17.5.236
     Houdini Engine Version: 3.2.40
     Unreal Version: 4.22.0
 
@@ -46,7 +46,7 @@ public class HoudiniEngineEditor : ModuleRules
 {
     private string GetHFSPath()
     {
-        string HoudiniVersion = "17.5.231";
+        string HoudiniVersion = "17.5.236";
         bool bIsRelease = true;
         string HFSPath = "";
         string RegistryPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Side Effects Software";
@@ -168,7 +168,7 @@ public class HoudiniEngineEditor : ModuleRules
     public HoudiniEngineEditor( ReadOnlyTargetRules Target ) : base( Target )
     {
         bPrecompile = true;
-        PCHUsage = PCHUsageMode.UseSharedPCHs;
+        PCHUsage = PCHUsageMode.NoSharedPCHs;
         PrivatePCHHeaderFile = "Private/HoudiniEngineEditorPrivatePCH.h";
 
         // Check if we are compiling on unsupported platforms.
--- Source/HoudiniEngineRuntime/HoudiniEngineRuntime.Build.cs
+++ Source/HoudiniEngineRuntime/HoudiniEngineRuntime.Build.cs
@@ -32,7 +32,7 @@
 
 /*
 
-    Houdini Version: 17.5.231
+    Houdini Version: 17.5.236
     Houdini Engine Version: 3.2.40
     Unreal Version: 4.22.0
 
@@ -46,7 +46,7 @@ public class HoudiniEngineRuntime : ModuleRules
 {
     private string GetHFSPath()
     {
-        string HoudiniVersion = "17.5.231";
+        string HoudiniVersion = "17.5.236";
         bool bIsRelease = true;
         string HFSPath = "";
         string RegistryPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Side Effects Software";
@@ -194,7 +194,7 @@ public class HoudiniEngineRuntime : ModuleRules
     public HoudiniEngineRuntime( ReadOnlyTargetRules Target ) : base( Target )
     {
         bPrecompile = true;
-        PCHUsage = PCHUsageMode.UseSharedPCHs;
+        PCHUsage = PCHUsageMode.NoSharedPCHs;
         PrivatePCHHeaderFile = "Private/HoudiniEngineRuntimePrivatePCH.h";
 
         // Check if we are compiling for unsupported platforms.
@@ -270,7 +270,7 @@ public class HoudiniEngineRuntime : ModuleRules
        PrivateDependencyModuleNames.AddRange(
             new string[]
             {
-                // ... add private dependencies that you statically link with here ...
+                "Landscape"
             }
        );
 
@@ -295,7 +295,6 @@ public class HoudiniEngineRuntime : ModuleRules
                     "TargetPlatform",
                     "UnrealEd",
                     "ApplicationCore",
-                    "Landscape",
                     "LandscapeEditor"
                 }
             );
