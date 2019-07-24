a29a6f0824303e17a61c212a89e15254c3f272b2
Damien Pernuit <damienp@sidefx.com>
2019-07-24 15:59:09 -0400

    Asset Inputs - Fixed forced recooks on downstream asset, bypassing the paused/disabled cooking state. - Fixed applying an HDA (via contect menu/houdini tools) to another HDA Actor causing the destruction of the input HDA on the HAPI side, forcing the input HDA to need a re-instantiation.

--- Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
@@ -1506,13 +1506,11 @@ UHoudiniAssetComponent::PostCook( bool bCookError )
         }
     }
 
-    // We can reset the manual recook flag now that the static meshes have been created
-    bManualRecookRequested = false;
-
     // Invoke cooks of downstream assets.
     if ( bCookingTriggersDownstreamCooks && DownstreamAssetConnections.Num() > 0)
     {
         // First, make sure our downstream assets are valid
+        // (check that the component is valid and we are properly set as one of its asset input)
         ValidateDownstreamAssets();
 
         for ( TMap<UHoudiniAssetComponent *, TSet< int32 > >::TIterator IterAssets( DownstreamAssetConnections );
@@ -1523,10 +1521,13 @@ UHoudiniAssetComponent::PostCook( bool bCookError )
             if ( !DownstreamAsset || DownstreamAsset->IsPendingKill() )
                 continue;
 
-            DownstreamAsset->bManualRecookRequested = true;
+            DownstreamAsset->bManualRecookRequested = bManualRecookRequested;
             DownstreamAsset->NotifyParameterChanged( nullptr );
         }
     }
+
+    // We can reset the manual recook flag now that the static meshes have been created
+    bManualRecookRequested = false;
 }
 
 void
