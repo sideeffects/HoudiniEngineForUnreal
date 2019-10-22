0a7280bc791bb27cf13e89388cf4cc1e1d152af8
Damien Pernuit <damienp@sidefx.com>
2019-10-22 11:14:17 -0400

    Bumping to Houdini Version: 18.5.7 Houdini Engine Version: 3.3 (API: 1)

--- Source/HoudiniEngineRuntime/Public/HAPI/HAPI_Version.h
+++ Source/HoudiniEngineRuntime/Public/HAPI/HAPI_Version.h
@@ -26,13 +26,13 @@
 // The three components of the Houdini version that HAPI is
 // expecting to compile against.
 #define HAPI_VERSION_HOUDINI_MAJOR 18
-#define HAPI_VERSION_HOUDINI_MINOR 0
-#define HAPI_VERSION_HOUDINI_BUILD 251
+#define HAPI_VERSION_HOUDINI_MINOR 5
+#define HAPI_VERSION_HOUDINI_BUILD 7
 #define HAPI_VERSION_HOUDINI_PATCH 0
 
 // The two components of the Houdini Engine (marketed) version.
 #define HAPI_VERSION_HOUDINI_ENGINE_MAJOR 3
-#define HAPI_VERSION_HOUDINI_ENGINE_MINOR 2
+#define HAPI_VERSION_HOUDINI_ENGINE_MINOR 3
 
 // This is a monotonously increasing API version number that can be used
 // to lock against a certain API for compatibility purposes. Basically,
@@ -40,6 +40,6 @@
 // might no longer compile. Semantic changes to the methods will also
 // cause this version to increase. This number will be reset to 0
 // every time the Houdini Engine version is bumped.
-#define HAPI_VERSION_HOUDINI_ENGINE_API 44
+#define HAPI_VERSION_HOUDINI_ENGINE_API 1
 
 #endif // __HAPI_VERSION_h__
