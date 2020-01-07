890796087de574ae403ee3f1cb82ac54537c5458
Damien Pernuit <damienp@sidefx.com>
2020-01-07 12:34:25 -0500

    Bumping to Houdini Version: 18.5.84 Houdini Engine Version: 3.3 (API: 2)

--- Source/HoudiniEngineRuntime/Public/HAPI/HAPI_Version.h
+++ Source/HoudiniEngineRuntime/Public/HAPI/HAPI_Version.h
@@ -27,7 +27,7 @@
 // expecting to compile against.
 #define HAPI_VERSION_HOUDINI_MAJOR 18
 #define HAPI_VERSION_HOUDINI_MINOR 5
-#define HAPI_VERSION_HOUDINI_BUILD 56
+#define HAPI_VERSION_HOUDINI_BUILD 84
 #define HAPI_VERSION_HOUDINI_PATCH 0
 
 // The two components of the Houdini Engine (marketed) version.
