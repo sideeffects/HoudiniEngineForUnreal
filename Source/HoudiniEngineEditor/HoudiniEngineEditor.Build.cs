e4bfd9f8f4c0bf1df4c5e0b7a26b4d37e8fd08d1
Damien Pernuit <damienp@sidefx.com>
2021-01-13 15:57:14 -0500

    Bumping to Houdini Version: 18.0.702 Houdini Engine Version: 3.4 (API: 0)

--- Source/HoudiniEngineEditor/HoudiniEngineEditor.Build.cs
+++ Source/HoudiniEngineEditor/HoudiniEngineEditor.Build.cs
@@ -32,8 +32,8 @@
 
 /*
 
-    Houdini Version: 18.0.409
-    Houdini Engine Version: 3.3.3
+    Houdini Version: 18.0.702
+    Houdini Engine Version: 3.4.0
     Unreal Version: 4.21.2
 
 */
@@ -46,7 +46,7 @@ public class HoudiniEngineEditor : ModuleRules
 {
     private string GetHFSPath()
     {
-        string HoudiniVersion = "18.0.409";
+        string HoudiniVersion = "18.0.702";
         bool bIsRelease = true;
         string HFSPath = "";
         string RegistryPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\WOW6432Node\\Side Effects Software";
--- Source/HoudiniEngineRuntime/Public/HAPI/HAPI_Version.h
+++ Source/HoudiniEngineRuntime/Public/HAPI/HAPI_Version.h
@@ -27,12 +27,12 @@
 // expecting to compile against.
 #define HAPI_VERSION_HOUDINI_MAJOR 18
 #define HAPI_VERSION_HOUDINI_MINOR 0
-#define HAPI_VERSION_HOUDINI_BUILD 409
+#define HAPI_VERSION_HOUDINI_BUILD 702
 #define HAPI_VERSION_HOUDINI_PATCH 0
 
 // The two components of the Houdini Engine (marketed) version.
 #define HAPI_VERSION_HOUDINI_ENGINE_MAJOR 3
-#define HAPI_VERSION_HOUDINI_ENGINE_MINOR 3
+#define HAPI_VERSION_HOUDINI_ENGINE_MINOR 4
 
 // This is a monotonously increasing API version number that can be used
 // to lock against a certain API for compatibility purposes. Basically,
@@ -40,6 +40,6 @@
 // might no longer compile. Semantic changes to the methods will also
 // cause this version to increase. This number will be reset to 0
 // every time the Houdini Engine version is bumped.
-#define HAPI_VERSION_HOUDINI_ENGINE_API 3
+#define HAPI_VERSION_HOUDINI_ENGINE_API 0
 
 #endif // __HAPI_VERSION_h__
