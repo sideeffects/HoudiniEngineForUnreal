5580df0c22d55f0ba59af554a0f8ae7d787ab0a6
Anthony Tu <anthonyt@sidefx.com>
2021-10-25 16:54:45 -0400

    Unreal Version 2: The generated mesh output now properly uses the collision complexity from the Houdini Mesh Generation Properties. A side effect of this is that the default collision complexity behaviour is now CTF_UseDefault instead of CTF_UseComplexAsSimple.

--- Source/HoudiniEngine/Private/HoudiniMeshTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniMeshTranslator.cpp
@@ -1508,7 +1508,7 @@ FHoudiniMeshTranslator::CreateStaticMesh_RawMesh()
 
 	UStaticMesh* MainStaticMesh = nullptr;
 	bool bAssignedCustomCollisionMesh = false;
-	ECollisionTraceFlag MainStaticMeshCTF = ECollisionTraceFlag::CTF_UseComplexAsSimple;
+	ECollisionTraceFlag MainStaticMeshCTF = StaticMeshGenerationProperties.GeneratedCollisionTraceFlag;
 
 	// Iterate through all detected split groups we care about and split geometry.
 	// The split are ordered in the following way:
@@ -2955,7 +2955,7 @@ FHoudiniMeshTranslator::CreateStaticMesh_MeshDescription()
 
 	UStaticMesh* MainStaticMesh = nullptr;
 	bool bAssignedCustomCollisionMesh = false;
-	ECollisionTraceFlag MainStaticMeshCTF = ECollisionTraceFlag::CTF_UseComplexAsSimple;
+	ECollisionTraceFlag MainStaticMeshCTF = StaticMeshGenerationProperties.GeneratedCollisionTraceFlag;
 
 	// Iterate through all detected split groups we care about and split geometry.
 	// The split are ordered in the following way:
