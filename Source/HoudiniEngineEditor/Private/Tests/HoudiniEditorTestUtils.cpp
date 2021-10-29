82f0f847223b094965e801cda9832f7cafe7f1d1
Damien Pernuit <damienp@sidefx.com>
2021-10-29 18:13:35 -0400

    Version 2 - Merging PR#171 on behalf of MarkovRoman.   Fixed Linux build error due to case issue for UE4

--- Source/HoudiniEngineEditor/Private/Tests/HoudiniEditorTestUtils.cpp
+++ Source/HoudiniEngineEditor/Private/Tests/HoudiniEditorTestUtils.cpp
@@ -51,7 +51,7 @@
 #include "LevelEditor.h"
 #include "AssetRegistryModule.h"
 #include "Core/Public/HAL/FileManager.h"
-#include "Core/Public/HAL/PlatformFileManager.h"
+#include "Core/Public/HAL/PlatformFilemanager.h"
 #include "Editor/EditorPerformanceSettings.h"
 #include "Engine/Selection.h"
 #include "Interfaces/IMainFrameModule.h"
