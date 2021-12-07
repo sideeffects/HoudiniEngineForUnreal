e5ebdc63ef6bf89138cdef7ce1f994c17571dffe
Van <vanaarde@sidefx.com>
2021-12-07 03:55:34 -0500

    Version 2 - Fix GeometryToolsEngine.h build issue - Fix issue where mesh translation would crash if an output node has a   template flag enabled.

--- Source/HoudiniEngine/Private/HoudiniMeshTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniMeshTranslator.cpp
@@ -408,7 +408,7 @@ FHoudiniMeshTranslator::CreateOrUpdateAllComponents(
 			}
 
 			// If the mesh we just created is templated, hide it in game
-			if (FoundHGPO->bIsTemplated)
+			if (IsValid(MeshComponent) && FoundHGPO->bIsTemplated)
 			{
 				MeshComponent->SetHiddenInGame(true);
 			}
@@ -442,6 +442,9 @@ FHoudiniMeshTranslator::UpdateMeshComponent(UMeshComponent *InMeshComponent, con
 		bool bVisible = InHGPO ? InHGPO->bIsVisible : true;
 		InMeshComponent->SetVisibility(bVisible);
 		InMeshComponent->SetHiddenInGame(!bVisible);
+		
+		FPropertyChangedEvent Evt(FindFieldChecked<FProperty>(InMeshComponent->GetClass(), "bVisible"));
+		InMeshComponent->PostEditChangeProperty(Evt);
 	}
 
 	// TODO:
@@ -6689,7 +6692,6 @@ FHoudiniMeshTranslator::PostCreateStaticMeshComponent(UStaticMeshComponent *InCo
 	if (Mesh)
 	{
 		InComponent->SetStaticMesh(Mesh);
-
 		return true;
 	}
 
