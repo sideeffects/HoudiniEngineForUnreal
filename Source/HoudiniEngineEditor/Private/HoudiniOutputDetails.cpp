dcc2337a7cf4baf3aa452408c683468245846d0a
Morne Chamberlain <mornechamberlain@sidefx.com>
2022-01-11 09:37:05 -0500

    Version 2
    Fixed a bug where baking a static mesh output that has an invisible complex
    collider split (a temporary static mesh as complex collider) would not also
    bake the collision mesh.

--- Source/HoudiniEngineEditor/Private/HoudiniOutputDetails.cpp
+++ Source/HoudiniEngineEditor/Private/HoudiniOutputDetails.cpp
@@ -3696,9 +3696,10 @@ FHoudiniOutputDetails::OnBakeOutputObject(
 			{
 				FDirectoryPath TempCookFolderPath;
 				TempCookFolderPath.Path = TempCookFolder;
+				TMap<UStaticMesh*, UStaticMesh*> AlreadyBakedStaticMeshMap;
 				TMap<UMaterialInterface *, UMaterialInterface *> AlreadyBakedMaterialsMap;
 				UStaticMesh* DuplicatedMesh = FHoudiniEngineBakeUtils::BakeStaticMesh(
-					StaticMesh, PackageParams, InAllOutputs, TempCookFolderPath, AlreadyBakedMaterialsMap, BakeStats);
+					StaticMesh, PackageParams, InAllOutputs, TempCookFolderPath, AlreadyBakedStaticMeshMap, AlreadyBakedMaterialsMap, BakeStats);
 			}
 		}
 		break;
