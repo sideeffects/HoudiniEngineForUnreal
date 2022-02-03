1b39d91ed7d105b10cf83d58d68e6b5ea686801e
Damien Pernuit <damienp@sidefx.com>
2022-02-03 11:48:53 -0500

    Version 2 - Merged PR#151 on behalf of Mystfit :   Fixed some RHS float type mismatches against the UE5-main branch. - Merged PR#148 on behalf of dearamy :   Fix UE5 Crash when input asset is a datatable.

--- Source/HoudiniEngine/Private/HoudiniInputTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniInputTranslator.cpp
@@ -3310,7 +3310,7 @@ FHoudiniInputTranslator::HapiCreateInputNodeForDataTable(const FString& InNodeNa
 
 		// Create an array
 		TArray<FString> ObjectPaths;
-		ObjectPaths.SetNumUninitialized(NumRows);
+		ObjectPaths.SetNum(NumRows);
 		for (int32 n = 0; n < ObjectPaths.Num(); n++)
 			ObjectPaths[n] = ObjectPathName;
 
