47146d426a223fae1b1bf8ef53eeeadde105639a
Damien Pernuit <damienp@sidefx.com>
2022-04-20 18:55:02 -0400

    Version 2 - Fixed Unreal Spline Component's closed state being incorrectly set when using world input. - Fixed world input ignoring the "Use Legacy Curve" setting when sending unreal spline components.   The setting was only used for (Houdini) curve inputs. - Enabled the "Use Legacy Curve" setting by default as it helps with instabilities when using curve inputs.

--- Source/HoudiniEngine/Private/HoudiniInputTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniInputTranslator.cpp
@@ -1406,7 +1406,7 @@ FHoudiniInputTranslator::UploadHoudiniInputObject(
 		case EHoudiniInputObjectType::SplineComponent:
 		{
 			UHoudiniInputSplineComponent* InputSpline = Cast<UHoudiniInputSplineComponent>(InInputObject);
-			bSuccess = FHoudiniInputTranslator::HapiCreateInputNodeForSplineComponent(ObjBaseName, InputSpline, InInput->GetUnrealSplineResolution());
+			bSuccess = FHoudiniInputTranslator::HapiCreateInputNodeForSplineComponent(ObjBaseName, InputSpline, InInput->GetUnrealSplineResolution(), InInput->IsUseLegacyInputCurvesEnabled());
 
 			if (bSuccess)
 				OutCreatedNodeIds.Add(InInputObject->InputObjectNodeId);
@@ -2367,7 +2367,11 @@ FHoudiniInputTranslator::HapiCreateInputNodeForInstancedStaticMeshComponent(
 }
 
 bool
-FHoudiniInputTranslator::HapiCreateInputNodeForSplineComponent(const FString& InObjNodeName, UHoudiniInputSplineComponent* InObject, const float& SplineResolution)
+FHoudiniInputTranslator::HapiCreateInputNodeForSplineComponent(
+	const FString& InObjNodeName,
+	UHoudiniInputSplineComponent* InObject,
+	const float& SplineResolution, 
+	const bool& bInUseLegacyInputCurves)
 {
 	if (!IsValid(InObject))
 		return false;
@@ -2383,7 +2387,7 @@ FHoudiniInputTranslator::HapiCreateInputNodeForSplineComponent(const FString& In
 
 	FString NodeName = InObjNodeName + TEXT("_") + InObject->GetName();
 
-	if (!FUnrealSplineTranslator::CreateInputNodeForSplineComponent(Spline, SplineResolution, InObject->InputNodeId, NodeName))
+	if (!FUnrealSplineTranslator::CreateInputNodeForSplineComponent(Spline, SplineResolution, InObject->InputNodeId, NodeName, bInUseLegacyInputCurves))
 		return false;
 
 	// Cache the exported curve's data to the input object
@@ -2417,7 +2421,7 @@ FHoudiniInputTranslator::HapiCreateInputNodeForSplineComponent(const FString& In
 
 bool
 FHoudiniInputTranslator::HapiCreateInputNodeForHoudiniSplineComponent(
-	const FString& InObjNodeName, UHoudiniInputHoudiniSplineComponent* InObject, bool bInAddRotAndScaleAttributes, bool bInUseLegacyInputCurves)
+	const FString& InObjNodeName, UHoudiniInputHoudiniSplineComponent* InObject, const bool& bInAddRotAndScaleAttributes, const bool& bInUseLegacyInputCurves)
 {
 	if (!IsValid(InObject))
 		return false;
@@ -2451,7 +2455,7 @@ FHoudiniInputTranslator::HapiCreateInputNodeForHoudiniSplineComponent(
 
 bool
 FHoudiniInputTranslator::
-HapiCreateInputNodeForHoudiniAssetComponent(const FString& InObjNodeName, UHoudiniInputHoudiniAsset* InObject, const bool bKeepWorldTransform, const bool& bImportAsReference, const bool& bImportAsReferenceRotScaleEnabled)
+HapiCreateInputNodeForHoudiniAssetComponent(const FString& InObjNodeName, UHoudiniInputHoudiniAsset* InObject, const bool& bKeepWorldTransform, const bool& bImportAsReference, const bool& bImportAsReferenceRotScaleEnabled)
 {
 	if (!IsValid(InObject))
 		return false;
