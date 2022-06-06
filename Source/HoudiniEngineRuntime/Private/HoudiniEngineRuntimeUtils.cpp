013cc71490fb6912012b84261a4a9c9ddd2cb4ff
Damien Pernuit <damienp@sidefx.com>
2022-06-06 17:28:20 -0400

    Version 2 - Fixed Landscape having a transform offset (centered) after being sent to Houdini

--- Source/HoudiniEngineRuntime/Private/HoudiniEngineRuntimeUtils.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniEngineRuntimeUtils.cpp
@@ -487,7 +487,8 @@ FHoudiniEngineRuntimeUtils::MarkBlueprintAsModified(UActorComponent* ComponentTe
 }
 #endif
 
-FTransform FHoudiniEngineRuntimeUtils::CalculateHoudiniLandscapeTransform(ULandscapeInfo* LandscapeInfo)
+FTransform 
+FHoudiniEngineRuntimeUtils::CalculateHoudiniLandscapeTransform(ULandscapeInfo* LandscapeInfo)
 {
 #if WITH_EDITOR
 	ALandscapeProxy* LandscapeProxy = LandscapeInfo->GetLandscapeProxy();
