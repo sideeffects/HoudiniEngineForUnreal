7ac5ddb1904a4483be0cd6099907d9be485e3129
Damien Pernuit <damienp@sidefx.com>
2022-08-04 20:51:53 -0400

    - Added missing include.

--- Source/HoudiniEngine/Private/HoudiniEngineUtils.cpp
+++ Source/HoudiniEngine/Private/HoudiniEngineUtils.cpp
@@ -92,6 +92,7 @@
 #if WITH_EDITOR
 	#include "EditorModeManager.h"
 	#include "EditorModes.h"
+	#include "EditorFramework/AssetImportData.h"
 #endif
 
 #define LOCTEXT_NAMESPACE HOUDINI_LOCTEXT_NAMESPACE
