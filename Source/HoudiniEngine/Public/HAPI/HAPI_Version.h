d51389eff2601d42e09a2c3258e7df9316b3a694
Jeffery Li <jefferyl@sidefx.com>
2022-08-12 14:50:27 -0400

    Bumping to Houdini Version: 19.5.341 Houdini Engine Version: 5.0 (API: 0)

--- Source/HoudiniEngine/Public/HAPI/HAPI_Version.h
+++ Source/HoudiniEngine/Public/HAPI/HAPI_Version.h
@@ -27,7 +27,7 @@
 // expecting to compile against.
 #define HAPI_VERSION_HOUDINI_MAJOR 19
 #define HAPI_VERSION_HOUDINI_MINOR 5
-#define HAPI_VERSION_HOUDINI_BUILD 334
+#define HAPI_VERSION_HOUDINI_BUILD 341
 #define HAPI_VERSION_HOUDINI_PATCH 0
 
 // The two components of the Houdini Engine (marketed) version.
