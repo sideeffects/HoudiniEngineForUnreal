b005daae21656ed6dac81baa18cd9eb4d5eaea2b
David Hua <davidh@sidefx.com>
2022-11-17 14:52:09 -0500

    Fix deletion of null pointer that was causing undefined behavior.

--- Source/HoudiniEngine/Private/HoudiniOutputTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniOutputTranslator.cpp
@@ -633,7 +633,9 @@ FHoudiniOutputTranslator::UpdateOutputs(
 				uint8* RowData = (uint8*) FMemory::MallocZeroed(NumRows * StructSize);
 				// FoundProps is the map of properties that need to be set in the data table
 				// They are guaranteed to have an attribute in the point cloud input.
-				TArray<void*> ExtraPointers;
+				TArray<FString*> ExtraStrings;
+				TArray<FText*> ExtraTexts;
+				TArray<FName*> ExtraNames;
 				Status = FHoudiniDataTableTranslator::PopulateRowData(GeoId,
 					PartId,
 					FoundProps,
@@ -641,10 +643,21 @@ FHoudiniOutputTranslator::UpdateOutputs(
 					StructSize,
 					NumRows,
 					RowData,
-					ExtraPointers);
+					RowStruct,
+					ExtraStrings,
+					ExtraTexts,
+					ExtraNames);
 				if (!Status)
 				{
-					for (auto&& Ptr : ExtraPointers)
+					for (const FString* Ptr : ExtraStrings)
+					{
+						delete Ptr;
+					}
+					for (const FText* Ptr : ExtraTexts)
+					{
+						delete Ptr;
+					}
+					for (const FName* Ptr : ExtraNames)
 					{
 						delete Ptr;
 					}
@@ -659,7 +672,15 @@ FHoudiniOutputTranslator::UpdateOutputs(
 					DataTableName,
 					RowStruct,
 					RowData);
-				for (auto&& Ptr : ExtraPointers)
+				for (const FString* Ptr : ExtraStrings)
+				{
+					delete Ptr;
+				}
+				for (const FText* Ptr : ExtraTexts)
+				{
+					delete Ptr;
+				}
+				for (const FName* Ptr : ExtraNames)
 				{
 					delete Ptr;
 				}
