b3071a644aa3265a611f95fcb52d6e5c865692b7
Damien Pernuit <damienp@sidefx.com>
2022-11-21 17:38:52 -0500

    - Fixed data table output being ignored if the attributes were present on a curve part. - Fixed data table output being ignored if the first attribute / column process was a string. - DT output: Replaced call to deprecated ImportText function with ImportText_direct.

--- Source/HoudiniEngine/Private/HoudiniOutputTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniOutputTranslator.cpp
@@ -448,6 +448,7 @@ FHoudiniOutputTranslator::UpdateOutputs(
 			bCreatedNewMaps |= bNewMapCreated;
 			break;
 		}
+
 		case EHoudiniOutputType::DataTable:
 		{
 			for (auto&& HGPO : CurOutput->HoudiniGeoPartObjects)
@@ -659,6 +660,7 @@ FHoudiniOutputTranslator::UpdateOutputs(
 			}
 			break;
 		}
+
 		default:
 			// Do Nothing for now
 			break;
@@ -1671,15 +1673,21 @@ FHoudiniOutputTranslator::BuildAllOutputs(
 							// Instancer type is set by IsAttributeInstancer
 							//CurrentInstancerType = EHoudiniInstancerType::OldSchoolAttributeInstancer;
 						}
+						else if (FHoudiniEngineUtils::IsValidDataTable(CurrentHapiGeoInfo.nodeId, CurrentHapiPartInfo.id))
+						{
+							// the curve is actually a data table!
+							CurrentPartType = EHoudiniPartType::DataTable;
+						}
 						else
 						{
 							// The curve is a curve!
 							CurrentPartType = EHoudiniPartType::Curve;
 						}
 					}
-						break;
+					break;
 
 					case HAPI_PARTTYPE_INSTANCER:
+					{
 						// This is a packed primitive instancer
 						CurrentPartType = EHoudiniPartType::Instancer;
 						if (!bIsGeometryCollection)
@@ -1690,8 +1698,8 @@ FHoudiniOutputTranslator::BuildAllOutputs(
 						{
 							CurrentInstancerType = EHoudiniInstancerType::GeometryCollection;
 						}
-					
-						break;
+					}
+					break;
 
 					case HAPI_PARTTYPE_VOLUME:
 						// Volume data, likely a Heightfield height / mask	
