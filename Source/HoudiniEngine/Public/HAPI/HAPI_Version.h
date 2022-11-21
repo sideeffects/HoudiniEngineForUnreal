373a32a606f85a37ee131e5739212c892e47dd83
Damien Pernuit <damienp@sidefx.com>
2022-11-21 17:28:31 -0500

    Bumping to Houdini Version: 20.0.169 Houdini Engine Version: 6.0 (API: 0)

--- Source/HoudiniEngine/Public/HAPI/HAPI_Version.h
+++ Source/HoudiniEngine/Public/HAPI/HAPI_Version.h
@@ -27,11 +27,11 @@
 // expecting to compile against.
 #define HAPI_VERSION_HOUDINI_MAJOR 20
 #define HAPI_VERSION_HOUDINI_MINOR 0
-#define HAPI_VERSION_HOUDINI_BUILD 165
+#define HAPI_VERSION_HOUDINI_BUILD 169
 #define HAPI_VERSION_HOUDINI_PATCH 0
 
 // The two components of the Houdini Engine (marketed) version.
-#define HAPI_VERSION_HOUDINI_ENGINE_MAJOR 5
+#define HAPI_VERSION_HOUDINI_ENGINE_MAJOR 6
 #define HAPI_VERSION_HOUDINI_ENGINE_MINOR 0
 
 // This is a monotonously increasing API version number that can be used
@@ -40,6 +40,6 @@
 // might no longer compile. Semantic changes to the methods will also
 // cause this version to increase. This number will be reset to 0
 // every time the Houdini Engine version is bumped.
-#define HAPI_VERSION_HOUDINI_ENGINE_API 1
+#define HAPI_VERSION_HOUDINI_ENGINE_API 0
 
 #endif // __HAPI_VERSION_h__
