701b29499cd0539e7d8a81e6317a3028190a3991
David Hua <davidh@sidefx.com>
2022-12-05 17:43:53 -0500

    Bumping to Houdini Version: 20.0.183 Houdini Engine Version: 6.0 (API: 0)

--- Source/HoudiniEngine/HoudiniEngine.Build.cs
+++ Source/HoudiniEngine/HoudiniEngine.Build.cs
@@ -32,7 +32,7 @@
 
 /*
 
-    Houdini Version: 20.0.180
+    Houdini Version: 20.0.183
     Houdini Engine Version: 6.0.0
     Unreal Version: 4.27.0
 
@@ -47,7 +47,7 @@ public class HoudiniEngine : ModuleRules
 {
     private string GetHFSPath()
     {
-        string HoudiniVersion = "20.0.180";
+        string HoudiniVersion = "20.0.183";
         bool bIsRelease = true;
         string HFSPath = "";
         string RegistryPath = "HKEY_LOCAL_MACHINE\\SOFTWARE\\Side Effects Software";
