4012e745870a64f0a4b6b589db3a7c24cec2a88e
David Hua <davidh@sidefx.com>
2022-12-05 17:51:29 -0500

    Backport of r433754 and r433756 from dev branch - fixed bug that would cause incorrect material assignments for high poly Houdini mesh inputs.

--- Source/HoudiniEngine/Private/UnrealMeshTranslator.cpp
+++ Source/HoudiniEngine/Private/UnrealMeshTranslator.cpp
@@ -3307,7 +3307,6 @@ FString FUnrealMeshTranslator::GetSimplePhysicalMaterialPath(UStaticMeshComponen
 		return StaticMesh->GetBodySetup()->PhysMaterial->GetPathName();
     return FString();
 }
-
 bool
 FUnrealMeshTranslator::CreateInputNodeForMeshDescription(
 	const HAPI_NodeId& NodeId,
@@ -3630,10 +3629,12 @@ FUnrealMeshTranslator::CreateInputNodeForMeshDescription(
 
 		int32 TriangleIdx = 0;
 		int32 VertexInstanceIdx = 0;
+
 		for (const FPolygonID &PolygonID : MDPolygons.GetElementIDs())
 		{
 			for (const FTriangleID &TriangleID : MeshDescription.GetPolygonTriangleIDs(PolygonID))
 			{
+
 				MeshTriangleVertexCounts[TriangleIdx] = 3;
 				for (int32 TriangleVertexIndex = 0; TriangleVertexIndex < 3; ++TriangleVertexIndex)
 				{
@@ -3744,12 +3745,13 @@ FUnrealMeshTranslator::CreateInputNodeForMeshDescription(
 				//--------------------------------------------------------------------------------------------------------------------- 
 				// TRIANGLE MATERIAL ASSIGNMENT
 				//---------------------------------------------------------------------------------------------------------------------
-				const FPolygonGroupID &PolygonGroupID = MeshDescription.GetPolygonPolygonGroup(PolygonID);
-				const int32 MaterialIndex = PolygonGroupToMaterialIndex.FindChecked(PolygonGroupID);
+				const FPolygonGroupID& PolygonGroupID = MeshDescription.GetPolygonPolygonGroup(PolygonID);
+				int32 MaterialIndex = PolygonGroupToMaterialIndex.FindChecked(PolygonGroupID);
 				TriangleMaterialIndices.Add(MaterialIndex);
 
 				TriangleIdx++;
 			}
+
 		}
 
 		// Now transfer valid vertex instance attributes to Houdini vertex attributes
@@ -4199,7 +4201,6 @@ FUnrealMeshTranslator::CreateInputNodeForMeshDescription(
 
 	return true;
 }
-
 
 void
 FUnrealMeshTranslator::CreateFaceMaterialArray(
@@ -4980,6 +4981,7 @@ FUnrealMeshTranslator::CreateHoudiniMeshAttributes(
 		FHoudiniEngine::Get().GetSession(),
 		NodeId, PartId, HAPI_UNREAL_ATTRIB_MATERIAL, &AttributeInfoMaterial))
 	{
+
 		// The New attribute has been successfully created, set its value
 		if (HAPI_RESULT_SUCCESS != FHoudiniEngineUtils::HapiSetAttributeStringData(
 			TriangleMaterials, NodeId, PartId, HAPI_UNREAL_ATTRIB_MATERIAL, AttributeInfoMaterial))
