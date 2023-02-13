fe6021e9499a65c07ba969d52224a130a9647a91
Damien Pernuit <damienp@sidefx.com>
2023-02-13 18:35:58 -0500

    - Fixed an issue when baking Static Mesh (packed "one instance instancers").   This caused the first mesh to be baked multiple times. - Added GetHACWorld() to pre-emptively fix issues when trying to access the HAC's world. - Fixed potential crash when loading a new map (due to GC).   This was caused by the plugin trying to access the input objects during GC/ saving for calculating input bounds. - Fixed potential ensure failure / crash with the PDG Asset Link when baking to Actors. - merged PR#221 from @yuhe00: Fix compilation error when building for Android platform with clang 14.0. - merged PR#230 from @fullike: Fix baking fails if OutputComponents has no elements.

--- Source/HoudiniEngine/Private/HoudiniPDGManager.cpp
+++ Source/HoudiniEngine/Private/HoudiniPDGManager.cpp
@@ -1709,9 +1709,6 @@ FHoudiniPDGManager::ProcessWorkItemResults()
 		const FHoudiniStaticMeshGenerationProperties& StaticMeshGenerationProperties = HAC ? HAC->StaticMeshGenerationProperties : FHoudiniEngineRuntimeUtils::GetDefaultStaticMeshGenerationProperties();
 		const FMeshBuildSettings& MeshBuildSettings = HAC ? HAC->StaticMeshBuildSettings : FHoudiniEngineRuntimeUtils::GetDefaultMeshBuildSettings();
 
-		// UWorld *World = ParentActor ? ParentActor->GetWorld() : AssetLink->GetWorld();
-		UWorld *World = AssetLink->GetWorld();
-
 		// .. All TOP Nets
 		for (UTOPNetwork* CurrentTOPNet : AssetLink->AllTOPNetworks)
 		{
