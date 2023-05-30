f2d5b413733660ac8a9f6f9eb367f8083d20d4b2
Damien Pernuit <damienp@sidefx.com>
2023-05-30 16:53:36 -0400

    - Added the ability to set a custom $HOME directory in the plugin settings.   That custom $HOME is then used by HAPI, but also Houdini/Session Sync when using the "Open In" commands.   This is helpful when having HE under source control, as it allows custom nodes to be detected properly.

--- Source/HoudiniEngine/Private/HoudiniEngine.cpp
+++ Source/HoudiniEngine/Private/HoudiniEngine.cpp
@@ -35,6 +35,7 @@
 #include "HoudiniEngineScheduler.h"
 #include "HoudiniEngineManager.h"
 #include "HoudiniEngineRuntime.h"
+#include "HoudiniEngineRuntimeUtils.h"
 #include "HoudiniEngineTask.h"
 #include "HoudiniEngineTaskInfo.h"
 #include "HoudiniAssetComponent.h"
@@ -620,6 +621,9 @@ FHoudiniEngine::StartSession(HAPI_Session*& SessionPtr,
 	// Set the HAPI_CLIENT_NAME environment variable to "unreal"
 	// We need to do this before starting HARS.
 	FPlatformMisc::SetEnvironmentVar(TEXT("HAPI_CLIENT_NAME"), TEXT("unreal"));
+
+	// Set custom $HOME env var if it's been specified
+	FHoudiniEngineRuntimeUtils::SetHoudiniHomeEnvironmentVariable();
 
 	HAPI_Result SessionResult = HAPI_RESULT_FAILURE;
 
