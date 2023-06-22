acd40808fd87f79307711de0a7988984ad77b39d
Damien Pernuit <damienp@sidefx.com>
2023-06-22 17:53:12 -0400

    - Fixed an issue that cause Unreal spline component to be deleted immediately after being sent when using the new input system. - Fixed a crash when setting up material for a specific slot on instancers, without having the attribute for the previous material slot.   ie, set unreal_material3 (for slot3) without having set unreal_material1 / unreal_material2

--- Source/HoudiniEngine/Private/HoudiniInputTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniInputTranslator.cpp
@@ -3052,6 +3052,7 @@ FHoudiniInputTranslator::HapiCreateInputNodeForSplineComponent(
 		return false;
 
 	// Cache the exported curve's data to the input object
+	InObject->InputNodeHandle = InputNodeHandle;
 	InObject->InputNodeId = CreatedNodeId;
 	InObject->InputObjectNodeId = FHoudiniEngineUtils::HapiGetParentNodeId(CreatedNodeId);
 
