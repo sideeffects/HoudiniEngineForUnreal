cc3af165fd923f07bdae740121e5ae1f3ef03fa6
Morne Chamberlain <mornechamberlain@sidefx.com>
2023-08-28 08:08:54 -0400

    When output actors/objects are created/deleted via the PDG asset link we now mark the relevant UTOPNode's package as dirty: this ensures that the outer level, or especially the outer HDA actor when using OFPA, is marked as dirty so that references to the output actors / objects are saved correctly.

--- Source/HoudiniEngine/Private/HoudiniPDGTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniPDGTranslator.cpp
@@ -117,10 +117,20 @@ FHoudiniPDGTranslator::CreateAllResultObjectsForPDGWorkItem(
 			if (!IsValid(TOPNodeOutputActor))
 			{
 				if (NodeOutputActorOwner.CreateOutputActor(World, InAssetLink, InAssetLink->OutputParentActor, FName(InTOPNode->NodeName)))
+				{
 					TOPNodeOutputActor = NodeOutputActorOwner.GetOutputActor();
+					// Ensure that the outer level (or actor in the case of OFPA) is marked as dirty so that references to the
+					// output actors / objects are saved
+					InTOPNode->MarkPackageDirty();
+				}
 			}
 			if (WROOutputActorOwner.CreateOutputActor(World, InAssetLink, TOPNodeOutputActor, FName(InWorkResultObject.Name)))
+			{
 				WorkItemOutputActor = WROOutputActorOwner.GetOutputActor();
+				// Ensure that the outer level (or actor in the case of OFPA) is marked as dirty so that references to the
+				// output actors / objects are saved
+				InTOPNode->MarkPackageDirty();
+			}
 		}
 
 		for (auto& OldOutput : OldTOPOutputs)
@@ -212,10 +222,20 @@ FHoudiniPDGTranslator::LoadExistingAssetsAsResultObjectsForPDGWorkItem(
 		if (!IsValid(TOPNodeOutputActor))
 		{
 			if (NodeOutputActorOwner.CreateOutputActor(World, InAssetLink, InAssetLink->OutputParentActor, FName(InTOPNode->NodeName)))
+			{
 				TOPNodeOutputActor = NodeOutputActorOwner.GetOutputActor();
+				// Ensure that the outer level (or actor in the case of OFPA) is marked as dirty so that references to the
+				// output actors / objects are saved
+				InTOPNode->MarkPackageDirty();
+			}
 		}
 		if (WROOutputActorOwner.CreateOutputActor(World, InAssetLink, TOPNodeOutputActor, FName(InWorkResultObject.Name)))
+		{
 			WorkItemOutputActor = WROOutputActorOwner.GetOutputActor();
+			// Ensure that the outer level (or actor in the case of OFPA) is marked as dirty so that references to the
+			// output actors / objects are saved
+			InTOPNode->MarkPackageDirty();
+		}
 	}
 
 	InWorkResultObject.SetResultOutputs(InOutputs);
