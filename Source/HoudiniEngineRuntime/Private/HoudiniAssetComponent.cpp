2511e25b87b84afc8b0ecde90b4120e05e3f21f9
Damien Pernuit <damienp@sidefx.com>
2023-10-24 18:58:13 -0400

    - Fixed the plugin being a little too aggressive when cleaning up its components after a duplication.   Components added in a BP template will no longer be deleted on PostDuplicate.

--- Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
@@ -1709,6 +1709,10 @@ UHoudiniAssetComponent::UpdatePostDuplicate()
 		if (!IsValid(NextChild))
 			continue;
 
+		// We don't want to remove components that were added in a Blueprint Template
+		if (NextChild->IsCreatedByConstructionScript())
+			continue;
+
 		USceneComponent * ComponentToRemove = nullptr;
 		if (NextChild->IsA<UStaticMeshComponent>()) 
 		{
