4194981d0a387e7cc97f8ad061b3e2ace319dd6f
Andy Styles <andys@sidefx.com>
2024-02-14 19:47:53 -0500

    Fixed crash when cooking with an HDA Input straddles a world partition boundary.

--- Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniAssetComponent.cpp
@@ -2106,12 +2106,17 @@ UHoudiniAssetComponent::GetAssetBounds(UHoudiniInput* IgnoreInput, bool bIgnoreG
 	*/
 
 	// Query the bounds for all our inputs
-	for (auto & CurInput : Inputs) 
+	// Bug: 134158: For some reason using inputs in this manner during cooking will crash the cooker
+	// when using World Partition. So ignore inputs during cooking.
+	if (!IsRunningCookCommandlet())
 	{
-		if (!IsValid(CurInput))
-			continue;
+		for (auto& CurInput : Inputs)
+		{
+			if (!IsValid(CurInput))
+				continue;
 
-		BoxBounds += CurInput->GetBounds(this->GetHACWorld());
+			BoxBounds += CurInput->GetBounds(this->GetHACWorld());
+		}
 	}
 
 	// Query the bounds for all input parameters
