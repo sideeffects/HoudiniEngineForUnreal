c3b29d52b0a7e344e6d0a5775404f4b8c7971cd1
Andy Styles <andys@sidefx.com>
2024-02-18 17:05:08 -0500

    Added profile timers

--- Source/HoudiniEngine/Private/UnrealLandscapeTranslator.cpp
+++ Source/HoudiniEngine/Private/UnrealLandscapeTranslator.cpp
@@ -1404,6 +1404,8 @@ FUnrealLandscapeTranslator::SetHeightfieldData(
 	const HAPI_VolumeInfo& VolumeInfo,
 	const FString& HeightfieldName)
 {
+	TRACE_CPUPROFILER_EVENT_SCOPE(FUnrealLandscapeTranslator::SetHeightfieldData);
+
 	// Cook the node to get proper infos on it
 	if(!FHoudiniEngineUtils::HapiCookNode(VolumeNodeId, nullptr, true))
 		return false;
@@ -1411,7 +1413,7 @@ FUnrealLandscapeTranslator::SetHeightfieldData(
 	// Read the geo/part/volume info from the volume node
 	HAPI_GeoInfo GeoInfo;
 	FHoudiniApi::GeoInfo_Init(&GeoInfo);
-	//FMemory::Memset< HAPI_GeoInfo >(GeoInfo, 0);
+
 	HOUDINI_CHECK_ERROR_RETURN(FHoudiniApi::GetGeoInfo(
 		FHoudiniEngine::Get().GetSession(),
 		VolumeNodeId, &GeoInfo), false);
