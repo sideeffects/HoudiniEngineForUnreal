bb160e1bac2ba172aa1b95bf1d7e7e6dcb36f649
Damien Pernuit <damienp@sidefx.com>
2024-07-30 15:55:33 -0400

    - Fixed Label parameters displaying column labels vertically instead of horizontally. - Fixed the Reset Parameters button being cut out and missing its icon.

--- Source/HoudiniEngineEditor/Private/HoudiniParameterDetails.cpp
+++ Source/HoudiniEngineEditor/Private/HoudiniParameterDetails.cpp
@@ -1000,7 +1000,7 @@ FHoudiniParameterDetails::CreateWidget(
 		}
 	}
 
-	// Remove a divider lines recurrsively if last joined parameter hits the end of a tab
+	// Remove a divider lines recursively if last joined parameter hits the end of a tab
 	{
 		const auto& LastLinkedParams = InJoinedParams.Last();
 		if (LastLinkedParams.IsEmpty())
@@ -1857,7 +1857,8 @@ FHoudiniParameterDetails::CreateWidgetFloat(
 			ChangeFloatValueAt(Val, 2, bDoChange, FloatParams);
 		};
 
-		VerticalBox->AddSlot().Padding(2, 2, 5, 2)
+		VerticalBox->AddSlot()
+		.Padding(2, 2, 5, 2)
 		[
 			SNew(SHorizontalBox)
 			+ SHorizontalBox::Slot()
@@ -1921,7 +1922,10 @@ FHoudiniParameterDetails::CreateWidgetFloat(
 			.VAlign(VAlign_Center)
 			[
 				SNew(SHorizontalBox)
-				+ SHorizontalBox::Slot().AutoWidth().HAlign(HAlign_Right).VAlign(VAlign_Center)
+				+ SHorizontalBox::Slot()
+				.AutoWidth()
+				.HAlign(HAlign_Right)
+				.VAlign(VAlign_Center)
 				[
 					SNew(SButton)
 					.ButtonStyle(_GetEditorStyle(), "NoBorder")
@@ -1951,7 +1955,10 @@ FHoudiniParameterDetails::CreateWidgetFloat(
 					})
 				]
 
-				+ SHorizontalBox::Slot().AutoWidth().HAlign(HAlign_Left).VAlign(VAlign_Center)
+				+ SHorizontalBox::Slot()
+				.AutoWidth()
+				.HAlign(HAlign_Left)
+				.VAlign(VAlign_Center)
 				[
 					SNew(SButton)
 					.ToolTipText(LOCTEXT("RevertToDefault", "Revert to default"))
@@ -1988,7 +1995,8 @@ FHoudiniParameterDetails::CreateWidgetFloat(
 			.Padding(2, 2, 5, 2)
 			[
 				SNew(SHorizontalBox)
-				+ SHorizontalBox::Slot().FillWidth(1.0f)
+				+ SHorizontalBox::Slot()
+				.FillWidth(1.0f)
 				[
 					SAssignNew(NumericEntryBox, SNumericEntryBox< float >)
 					.AllowSpin(true)
@@ -2940,25 +2948,29 @@ FHoudiniParameterDetails::CreateWidgetLabel(
 	if (!IsValidWeakPointer(MainParam))
 		return;
 
-	TSharedRef<SVerticalBox> VerticalBox = SNew(SVerticalBox);
+	TSharedRef<SHorizontalBox> HorizontalBox = SNew(SHorizontalBox);
 
 	for (int32 Index = 0; Index < MainParam->GetTupleSize(); ++Index)
 	{
 		FString NextLabelString = MainParam->GetStringAtIndex(Index);
 		FText ParameterLabelText = FText::FromString(NextLabelString);
+
+		FText ParamTooltipText = FText::FromString("Column " + FString::FromInt(Index) + ": " + NextLabelString);
 		
 		TSharedPtr<STextBlock> TextBlock;
-
 		// Add Label UI.
-		VerticalBox->AddSlot().Padding(1, 2, 16, 2)
+		HorizontalBox->AddSlot()
+		.Padding(1, 2, 16, 2)
+		.AutoWidth()
 		[
 			SAssignNew(TextBlock, STextBlock)
 			.Text(ParameterLabelText)
+			.ToolTipText(ParamTooltipText)
 			.Font(_GetEditorStyle().GetFontStyle(TEXT("PropertyWindow.NormalFont")))
 		];
 	}
 
-	LabelledParameter->SetContent(VerticalBox);
+	LabelledParameter->SetContent(HorizontalBox);
 }
 
 void
