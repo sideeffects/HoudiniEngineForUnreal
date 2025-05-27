94559baa28aa5791853bccc643a0c1cd4aded5d6
Damien Pernuit <damienp@sidefx.com>
2025-05-27 15:18:36 -0400

    - Fixed crash after multiple Play In Editor sessions, or when PIE after refining proxies.

--- Source/HoudiniEngine/Private/HoudiniEngineUtils.cpp
+++ Source/HoudiniEngine/Private/HoudiniEngineUtils.cpp
@@ -8687,11 +8687,10 @@ FHoudiniEngineUtils::TriageHoudiniCookablesForProxyMeshRefinement(
 						if(!IsValid(ProxyObject))
 							continue;
 
+						// Just mark the object as garbage and his package as dirty
+						// Do not save the package automatically - as will cause crashes in PIE
 						ProxyObject->MarkAsGarbage();
 						ProxyObject->MarkPackageDirty();
-						UPackage* const Package = ProxyObject->GetPackage();
-						if(IsValid(Package))
-							ProxyMeshPackagesToSave.Add(Package);
 					}
 				}
 			}
@@ -8723,14 +8722,6 @@ FHoudiniEngineUtils::TriageHoudiniCookablesForProxyMeshRefinement(
 		if(ProxyMeshPackagesToSave.Num() > 0)
 		{
 			TryCollectGarbage(GARBAGE_COLLECTION_KEEPFLAGS);
-
-			// TODO: Try to prevent potential crash upon multiple successive PIE
-			for (int nIdx = ProxyMeshPackagesToSave.Num() - 1; nIdx >= 0; nIdx--)
-			{
-				UPackage* CurPackage = ProxyMeshPackagesToSave[nIdx];
-				if (!IsValid(CurPackage) || !CurPackage->IsDirty())
-					ProxyMeshPackagesToSave.RemoveAt(nIdx);
-			}
 
 			FEditorFileUtils::PromptForCheckoutAndSave(ProxyMeshPackagesToSave, true, false);
 		}
