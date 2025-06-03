b789909114a4c7f41b38c0df05dea49462be638f
Damien Pernuit <damienp@sidefx.com>
2025-06-03 19:26:45 -0400

    - Fixed 5.6 non-editor build error due to MetaData no longer existing in non editor builds...

--- Source/HoudiniEngineRuntime/Private/HoudiniPDGAssetLink.cpp
+++ Source/HoudiniEngineRuntime/Private/HoudiniPDGAssetLink.cpp
@@ -1972,6 +1972,8 @@ FTOPWorkResultObject::DestroyResultOutputs(const FGuid& InHoudiniComponentGuid)
 				}
 				else
 				{
+// Technically this should be WITH_METADATA but it's 5.6 only..
+#if WITH_EDITORONLY_DATA
 					// ... if not an actor, destroy the object if it is a temp object created by the owning
 					// HoudiniAssetComponent. Don't delete anything if we don't have a valid component GUID.
 					if (IsValid(OutputObject.OutputObject) && !OutputObject.OutputObject->HasAnyFlags(RF_Transient) &&
@@ -2020,6 +2022,7 @@ FTOPWorkResultObject::DestroyResultOutputs(const FGuid& InHoudiniComponentGuid)
 							}
 						}
 					}
+#endif
 					OutputObject.OutputObject = nullptr;
 				}
 			}
