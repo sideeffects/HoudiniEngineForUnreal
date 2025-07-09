fb3dfcd8f060385f1d39faaa4f783ac04f52732d
Damien Pernuit <damienp@sidefx.com>
2025-07-09 10:31:36 -0400

    - Fixed build error on Mac due to a bad usage of TCHAR_TO_UTF8.

--- Source/HoudiniEngine/Private/UnrealPCGDataTranslator.cpp
+++ Source/HoudiniEngine/Private/UnrealPCGDataTranslator.cpp
@@ -481,8 +481,7 @@ void FUnrealPCGDataTranslator::SendToHoudini(UHoudiniPCGDataAttributeVector2d * 
 void FUnrealPCGDataTranslator::SendToHoudini(UHoudiniPCGDataAttributeVector3d * Data, HAPI_NodeId InputNodeId, HAPI_PartId PartId, HAPI_AttributeOwner Owner)
 {
 	HAPI_AttributeInfo AttrInfo;
-	char* AttrName = TCHAR_TO_UTF8((*GetHapiName(Data->GetAttrName())));
-	FHoudiniHapiAccessor Accessor(InputNodeId, PartId, AttrName);
+	FHoudiniHapiAccessor Accessor(InputNodeId, PartId, TCHAR_TO_UTF8((*GetHapiName(Data->GetAttrName()))));
 	Accessor.AddAttribute(Owner, HAPI_StorageType::HAPI_STORAGETYPE_FLOAT, 3, Data->Values.Num(), &AttrInfo);
 
 	TArray<float> FloatValues;
