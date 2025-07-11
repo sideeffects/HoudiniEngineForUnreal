fad910dd6190e630d17be36af9ca5ce52f8aaa2d
Damien Pernuit <damienp@sidefx.com>
2025-07-11 11:27:48 -0400

    - Fixed potential crash due to empty UV sets when outputting   multiple split meshes.

--- Source/HoudiniEngine/Private/HoudiniMeshTranslator.cpp
+++ Source/HoudiniEngine/Private/HoudiniMeshTranslator.cpp
@@ -2797,10 +2797,12 @@ FHoudiniMeshTranslator::CreateStaticMesh_MeshDescription()
 			bool bHasRGBA = bHasRGB && AttribInfoColors.tupleSize == 4;
 			bool bHasAlpha = SplitAlphas.Num() > 0;
 
+			// Bug #147854
+			// Use SplitUVs here - as that where we'll read UVs from
 			TArray<bool> HasUVSets;
-			HasUVSets.SetNumZeroed(PartUVSets.Num());
-			for (int32 Idx = 0; Idx < PartUVSets.Num(); Idx++)
-				HasUVSets[Idx] = PartUVSets[Idx].Num() > 0;
+			HasUVSets.SetNumZeroed(SplitUVSets.Num());
+			for (int32 Idx = 0; Idx < SplitUVSets.Num(); Idx++)
+				HasUVSets[Idx] = SplitUVSets[Idx].Num() > 0;
 
 			uint32 FaceCount = SplitIndices.Num() / 3;
 			for (uint32 FaceIndex = 0; FaceIndex < FaceCount; FaceIndex++)
@@ -6273,7 +6275,8 @@ FHoudiniMeshTranslator::UpdateMeshBuildSettings(
 }
 
 
-void FHoudiniMeshTranslator::BuildMeshDescription(FMeshDescription* MeshDescription, FHoudiniGroupedMeshPrimitives& SplitMeshData)
+void 
+FHoudiniMeshTranslator::BuildMeshDescription(FMeshDescription* MeshDescription, FHoudiniGroupedMeshPrimitives& SplitMeshData)
 {
 	bool bHasNormal = SplitMeshData.Normals.Num() > 0;
 	bool bHasTangents = SplitMeshData.TangentU.Num() > 0 && SplitMeshData.TangentV.Num() > 0;
@@ -6357,10 +6360,12 @@ void FHoudiniMeshTranslator::BuildMeshDescription(FMeshDescription* MeshDescript
 	TVertexInstanceAttributesRef<FVector2f> VertexInstanceUVs = MeshDescription->VertexInstanceAttributes().GetAttributesRef<FVector2f>(MeshAttribute::VertexInstance::TextureCoordinate);
 	VertexInstanceUVs.SetNumChannels(UVSetCount);
 
+	// Bug #147854
+	// Use SplitUVs here - as that where we'll read UVs from
 	TArray<bool> HasUVSets;
-	HasUVSets.SetNumZeroed(PartUVSets.Num());
-	for (int32 Idx = 0; Idx < PartUVSets.Num(); Idx++)
-		HasUVSets[Idx] = PartUVSets[Idx].Num() > 0;
+	HasUVSets.SetNumZeroed(SplitMeshData.UVSets.Num());
+	for (int32 Idx = 0; Idx < SplitMeshData.UVSets.Num(); Idx++)
+		HasUVSets[Idx] = SplitMeshData.UVSets[Idx].Num() > 0;
 
 	for (uint32 FaceIndex = 0; FaceIndex < FaceCount; FaceIndex++)
 	{
