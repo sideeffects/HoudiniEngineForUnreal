27c1ae5630e84358ef4f9ae952b1cae98fcdcc38
Andy Styles <andys@sidefx.com>
2025-07-23 13:47:02 -0400

    Disabale tests for 5.6

--- Source/HoudiniEngineEditor/Private/Tests/HoudiniEditorHeightfieldTests.cpp
+++ Source/HoudiniEngineEditor/Private/Tests/HoudiniEditorHeightfieldTests.cpp
@@ -38,7 +38,7 @@ IMPLEMENT_SIMPLE_HOUDINI_AUTOMATION_TEST(HoudiniEditorHeightfieldTest_Heightfiel
 
 bool HoudiniEditorHeightfieldTest_Heightfields_Common::RunTest(const FString & Parameters)
 {
-#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION == 6
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION < 6
 	FHoudiniEditorTestUtils::InitializeTests(this, [this]
 	{
 		const FString MapName = FHoudiniEditorHeightfieldTests::EquivalenceTestMapName;
@@ -56,7 +56,7 @@ IMPLEMENT_SIMPLE_HOUDINI_AUTOMATION_TEST(HoudiniEditorHeightfieldTest_hf_tile_sc
 
 bool HoudiniEditorHeightfieldTest_hf_tile_scatter_instance::RunTest(const FString & Parameters)
 {
-#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION == 6
+#if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION < 6
 	FHoudiniEditorTestUtils::InitializeTests(this, [this]
         {
                 const FString MapName = FHoudiniEditorHeightfieldTests::EquivalenceTestMapName;
