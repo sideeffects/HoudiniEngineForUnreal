24bf7e4e0a38b508128b17583a85fc59c46fdbb1
Damien Pernuit <damienp@sidefx.com>
2026-02-11 15:44:50 -0500

    Bumping to Houdini Version: 22 .0 .230 Houdini Engine Version: 9.0 (API: 0)

--- Source/HoudiniEngine/Public/HAPI/HAPI_Version.h
+++ Source/HoudiniEngine/Public/HAPI/HAPI_Version.h
@@ -29,7 +29,7 @@
 
 #define HAPI_VERSION_HOUDINI_MINOR 0
 
-#define HAPI_VERSION_HOUDINI_BUILD 229
+#define HAPI_VERSION_HOUDINI_BUILD 230
 #define HAPI_VERSION_HOUDINI_PATCH 0
 
 // The two components of the Houdini Engine (marketed) version.
