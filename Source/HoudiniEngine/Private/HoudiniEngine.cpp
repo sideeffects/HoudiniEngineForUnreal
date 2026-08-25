434b606a30b0af6655be8ce4290549fcbb366df6
Damien Pernuit <damienp@sidefx.com>
2026-08-25 15:46:32 -0400

    - Fixed build break due to remaining call to DisablePerfMon after r552338

--- Source/HoudiniEngine/Private/HoudiniEngine.cpp
+++ Source/HoudiniEngine/Private/HoudiniEngine.cpp
@@ -783,9 +783,6 @@ FHoudiniEngine::StartSessionInternal(
 		{
 			if(!bUseSessionSyncForAutomaticServer)
 			{
-				// TODO: remove me when the race condition in H is fixed.
-				DisablePerfMon();
-
 				UpdatePathForServer();
 				HAPI_ProcessId ServerProcID = -1;
 				HAPI_Result ServerResult = FHoudiniApi::StartThriftSharedMemoryServer(
